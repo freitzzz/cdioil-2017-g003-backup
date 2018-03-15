a2ea35f51ae44e0f9e846add3409af5111e85071
Gil Durão <1160936@isep.ipp.pt>
2018-03-15 15:33:31 +0000

    Testes unitários em falta da classe Admin.

--- core/src/test/java/cdioil/domain/authz/AdminTest.java
+++ core/src/test/java/cdioil/domain/authz/AdminTest.java
@@ -2,6 +2,7 @@ package cdioil.domain.authz;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  * Testes unitarios da classe Admin.
@@ -10,20 +11,76 @@ import static org.junit.Assert.*;
  */
 public class AdminTest {
 
+    private SystemUser sysUser;
+
+    @Before
+    public void setUp() {
+        Email email = new Email("lilpump@guccigang.com");
+        Password pwd = new Password("Bananas19");
+        Nome nome = new Nome("Lil", "Pump");
+        sysUser = new SystemUser(email, nome, pwd);
+    }
+
     /**
      * Teste para o construtor da classe Admin.
      */
     @Test
     public void testeConstrutor() {
         System.out.println("Testes Construtor");
-        Email email = new Email("lilpump@guccigang.com");
-        Password pwd = new Password("Bananas19");
-        Nome nome = new Nome("Lil","Pump");
-        SystemUser sysUser = new SystemUser(email,nome,pwd);
         assertNull("A condição deve acertar pois os argumentos são inválidos",
                 createAdmin(null));
         assertNotNull("A condição deve acertar pois os argumentos são válidos",
                 createAdmin(sysUser));
+    }
+
+    /**
+     * Teste para o metodo hashCode da classe Admin.
+     */
+    @Test
+    public void testeHashCode() {
+        System.out.println("hashCode");
+        Admin a = createAdmin(sysUser);
+        int expResult = sysUser.hashCode();
+        int result = a.hashCode();
+    }
+
+    /**
+     * Teste para o metodo equals da classe Admin.
+     */
+    @Test
+    public void testeEquals() {
+        System.out.println("equals");
+        Email email = new Email("bananas@bananeira.com");
+        Password pwd = new Password("Bananas19");
+        Nome nome = new Nome("Lil", "Pump");
+        SystemUser sysUser2 = new SystemUser(email, nome, pwd);
+        Admin a = createAdmin(sysUser);
+        Admin a2 = createAdmin(sysUser2);
+        Admin a3 = createAdmin(sysUser);
+
+        assertEquals("A condição deve acertar pois estamos a comparar"
+                + "as mesmas instancias", a, a);
+        assertNotEquals("A condição deve acertar pois estamos a comparar"
+                + "instancias de classes diferentes", a, "bananas");
+        assertNotEquals("A condição deve acertar pois estamos a comparar"
+                + "uma instancia com outra a null", a, null);
+        assertEquals("A condição deve acertar pois estamos a comparar"
+                + "duas instancias iguais", a, a3);
+        assertNotEquals("A condição deve acertar pois estamos a comparar"
+                + "duas instancias diferentes", a, a2);
+    }
+
+    /**
+     * Teste do metodo toString da classe Admin.
+     */
+    @Test
+    public void testeToString() {
+        System.out.println("toString");
+        Admin a = createAdmin(sysUser);
+        String expResult = sysUser.toString();
+        String result = a.toString();
+        assertEquals("A condição deve acertar pois as strings são iguais",
+                 expResult, result);
     }
 
     /**
