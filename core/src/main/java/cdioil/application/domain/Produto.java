9c952a46b43959a88d6b450e7973be071325783a
António Sousa [1161371] <1161371@isep.ipp.pt>
2018-03-15 12:13:59 +0000

    Adicionar Entidade Produto.

---
+++ core/src/main/java/cdioil/application/domain/Produto.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdioil.application.domain;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author António Sousa [1161371]
+ */
+@Entity
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "IDPRODUTO")
+    private Long id;
+
+    /**
+     * Nome do produto.
+     */
+    @Column(name = "NOME")
+    private String nome;
+
+    /**
+     * Construtor protegido apenas para uso de JPA.
+     */
+    protected Produto() {
+
+    }
+
+}
