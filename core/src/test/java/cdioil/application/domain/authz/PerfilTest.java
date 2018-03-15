f09c7c77d735c83c3e17253270195a6d04fb889b
João Moreira <1160928@isep.ipp.pt>
2018-03-15 14:19:38 +0000

    Adicionadas classes de teste para as classes Avaliacao e Perfil. Implementada classe MudarInfoUtilizadorController.

---
+++ core/src/test/java/cdioil/application/domain/authz/PerfilTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdioil.application.domain.authz;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author João
+ */
+public class PerfilTest {
+
+    /**
+     * Test of adicionarAvaliacao method, of class Perfil.
+     */
+    /*@Test
+    public void testAdicionarAvaliacao() {
+        System.out.println("adicionarAvaliacao");
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+    /**
+     * Test of mudarInformacao method, of class Perfil.
+     */
+    /*@Test
+    public void testMudarInformacao() {
+        System.out.println("mudarInformacao");
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+
+}
