7cc6ed99d9831a475c42399df601ec8b19e1284b
Joana Pinheiro [1161380] <1161380@isep.ipp.pt>
2018-03-15 12:53:16 +0000

    Classe RepositorioUtilizadoresJPA adicionada

---
+++ core/src/main/java/cdioil/application/persistence/RepositorioUtilizadoresJPA.java
@@ -0,0 +1,16 @@
+package cdioil.application.persistence;
+
+import cdioil.domain.authz.UserRegistado;
+
+public class RepositorioUtilizadoresJPA extends RepositorioBaseJPA<UserRegistado, Long> implements RepositorioUtilizadores {
+
+    @Override
+    protected String nomeUnidadePersistencia() {
+        return null;
+    }
+
+    @Override
+    public UserRegistado addUserRegistado(UserRegistado userRegistado) {
+        return (UserRegistado) super.add(userRegistado);
+    }
+}
