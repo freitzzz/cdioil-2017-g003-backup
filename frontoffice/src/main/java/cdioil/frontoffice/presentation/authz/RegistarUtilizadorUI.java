4d0e7ae7dc267c001fb6ec1be7ef398301d1610c
Joana Pinheiro [1161380] <1161380@isep.ipp.pt>
2018-03-15 15:09:30 +0000

    RegistarUtilizadorUI adicionado

--- frontoffice/src/main/java/cdioil/frontoffice/presentation/authz/RegistarUtilizadorUI.java
+++ frontoffice/src/main/java/cdioil/frontoffice/presentation/authz/RegistarUtilizadorUI.java
@@ -1,4 +1,20 @@
 package cdioil.frontoffice.presentation.authz;
 
+
+import cdioil.application.authz.RegistarUtilizadorController;
+import cdioil.frontoffice.utils.Console;
+
 public class RegistarUtilizadorUI {
+
+    private RegistarUtilizadorController controller = new RegistarUtilizadorController();
+
+
+    public void registarUtilizadorUI() {
+        System.out.println("**** Registar Utilizadores ****\n");
+        String primeiroNome = Console.readLine("\nNome: ");
+        String apelido = Console.readLine("\nApelido: ");
+        String email = Console.readLine("\nEmail: ");
+
+        controller.criarUtilizadorRgistado(primeiroNome, apelido, email, Console.readLine("\nPassword: "));
+    }
 }
