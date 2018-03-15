636c9fe562b61d90b418ff087b77c343916588b6
Joana Pinheiro [1161380] <1161380@isep.ipp.pt>
2018-03-15 12:51:34 +0000

    Classe RegistarUtilizadorController Adicionada

---
+++ core/src/main/java/cdioil/application/authz/RegistarUtilizadorController.java
@@ -0,0 +1,18 @@
+package cdioil.application.authz;
+
+import cdioil.application.persistence.RepositorioUtilizadoresJPA;
+import cdioil.domain.authz.*;
+
+
+public class RegistarUtilizadorController {
+
+    private RepositorioUtilizadoresJPA repositorioUtilizadoresJPA = new RepositorioUtilizadoresJPA();
+
+
+    public void criarUtilizadorRgistado(String primeiroNome, String apelido, String email, String password) {
+        SystemUser systemUser = new SystemUser(new Email(email), new Nome(primeiroNome, apelido), new Password(password));
+        UserRegistado userRegistado = new UserRegistado(systemUser);
+
+        repositorioUtilizadoresJPA.addUserRegistado(userRegistado);
+    }
+}
