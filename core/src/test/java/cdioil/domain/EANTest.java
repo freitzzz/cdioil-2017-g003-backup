423fa8dacbb0f16730e347ebd22314408c54a0ef
António Sousa [1161371] <1161371@isep.ipp.pt>
2018-03-21 09:22:54 +0000

    Atualizar testes unitários das classes concretas de códigos.

--- core/src/test/java/cdioil/domain/EANTest.java
+++ core/src/test/java/cdioil/domain/EANTest.java
@@ -5,22 +5,21 @@
  */
 package cdioil.domain;
 
-import cdioil.domain.CodigoQR;
-import cdioil.domain.EAN;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 /**
  * Teste da classe EAN.
+ *
  * @author Ana Guerra (1161191)
  */
 public class EANTest {
-    
+
     /**
      * Instância de EAN para testes.
      */
-    EAN ean = new EAN(1700034560);
+    EAN ean = new EAN("1700034560");
 
     /**
      * Teste do método toString, da classe EAN.
@@ -28,7 +27,10 @@ public class EANTest {
     @Test
     public void testToString() {
         System.out.println("toString");
-        assertEquals("Os identificadores são iguais", "ID: 1700034560\n", ean.toString());
+        
+        EAN copia = new EAN("1700034560");
+
+        assertEquals("Os identificadores são iguais", copia.toString(), ean.toString());
     }
 
     /**
@@ -37,7 +39,10 @@ public class EANTest {
     @Test
     public void testHashCode() {
         System.out.println("hashCode");
-        assertEquals("Hash codes iguais", 1700035141, ean.hashCode());
+
+        EAN copia = new EAN("1700034560");
+
+        assertEquals("Hash codes iguais", copia.hashCode(), ean.hashCode());
     }
 
     /**
@@ -47,9 +52,9 @@ public class EANTest {
     public void testEquals() {
         System.out.println("equals");
         assertNotEquals("Objeto null não é igual", null, ean);
-        assertNotEquals("Instância de outra classe não é igual", new CodigoQR(1), ean);
-        assertNotEquals("Instância de EAN diferente", new EAN(60007896), ean);
-        assertEquals("Instância de EAN igual", new EAN(1700034560), ean);
+        assertNotEquals("Instância de outra classe não é igual", new CodigoQR("1"), ean);
+        assertNotEquals("Instância de EAN diferente", new EAN("60007896"), ean);
+        assertEquals("Instância de EAN igual", new EAN("1700034560"), ean);
     }
-    
+
 }
