7d555368f670868d3a1ee1b261c9bc9ac1f51eef
Ana Margarida Tavares Guerra <1161191@isep.ipp.pt>
2018-03-23 11:13:09 +0000

    Refractoring of the class Concurso - issue #12.

---
+++ core/src/main/java/cdioil/domain/Contest.java
@@ -0,0 +1,138 @@
+package cdioil.domain;
+
+import cdioil.domain.authz.GrupoUtilizadores;
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.*;
+
+/**
+ * Represents the contest which occurs for a group of app users over a period of time.
+ *
+ * @author <a href="1160936@isep.ipp.pt">Gil Durão</a>
+ */
+@Entity
+public class Contest implements Serializable, Evento {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    /**
+     * Id of the database to persist contest.
+     */
+    private Long id;
+
+    /**
+     * Description of the contest.
+     */
+    private String description;
+    /**
+     * Users to whom the contest is intended.
+     */
+    private GrupoUtilizadores users;
+    /**
+     * Begind date of the contest.
+     */
+    @Temporal(TemporalType.DATE)
+    private Calendar beginDate;
+    /**
+     * End date of the contest.
+     */
+    @Temporal(TemporalType.DATE)
+    private Calendar endDate;
+
+    /**
+     * Construt a instance of the Constest recibe the description, a group of the users,
+     * begind date and end date.
+     *
+     * @param description description of the contest
+     * @param users group of users for whom the Constest is
+     * @param beginDate begin date of the contest
+     * @param endDate end date of the contest
+     */
+    public Contest(String description, GrupoUtilizadores users,
+            Calendar beginDate, Calendar endDate) {
+        if (description == null) {
+            throw new IllegalArgumentException("O concurso tem que ter uma "
+                    + "descrição.");
+        }
+
+        if (beginDate == null) {
+            throw new IllegalArgumentException("O concurso tem que ter uma data"
+                    + " de inicio.");
+        }
+
+        if (endDate == null) {
+            throw new IllegalArgumentException("O concurso tem que ter uma data"
+                    + "de fecho.");
+        }
+        this.description = description;
+        this.users = users;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+
+    }
+
+    protected Contest() {
+        //Para ORM
+    }
+
+   /**
+     * Generates an unique index for the Constest.
+     *
+     * @return the hash value
+     */
+    @Override
+    public int hashCode() {
+        return description.hashCode();
+    }
+
+    /**
+     * Compares a Constest with another Object.
+     *
+     * @param object Object to compare
+     * @return true, if the two Objects are equal. Otherwise, returns false
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof Contest)) {
+            return false;
+        }
+        Contest other = (Contest) object;
+        return this.description.equals(other.description);
+    }
+
+    /**
+     * Describes a Constest through its description.
+     *
+     * @return description of the Constest
+     */
+    @Override
+    public String toString() {
+        return description;
+    }
+
+    /**
+     * Returns teh information of the Constest.
+     *
+     * @return the information of the constest with a String
+     */
+    @Override
+    public String info() {
+        return toString();
+    }
+
+    /**
+     * Returns the group of users for whom the Constest is.
+     *
+     * @return group of users
+     */
+    @Override
+    public GrupoUtilizadores publicoAlvo() {
+        return users;
+    }
+
+}
