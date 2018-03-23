9137af88afcfd5c28b13c22e1d87aaebf607ea40
António Sousa [1161371] <1161371@isep.ipp.pt>
2018-03-23 11:54:02 +0000

    Add empty constructor for concrete BinaryQuestion class.

--- core/src/main/java/cdioil/domain/BinaryQuestion.java
+++ core/src/main/java/cdioil/domain/BinaryQuestion.java
@@ -1,10 +1,6 @@
 package cdioil.domain;
 
-import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -16,6 +12,12 @@ import javax.persistence.UniqueConstraint;
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "QUESTION"))
 public class BinaryQuestion extends Question<String> {
+
+    /**
+     * Empty Constructor for JPA.
+     */
+    protected BinaryQuestion() {
+    }
     
     /**
      * Builds an instance of BinaryQuestion receiving a question.
