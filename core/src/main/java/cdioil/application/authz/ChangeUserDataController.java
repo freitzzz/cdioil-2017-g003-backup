bd4a41aec833ab19a9b6fa134aef9f1bb14d8c8e
João Moreira <1160928@isep.ipp.pt>
2018-03-23 09:39:37 +0000

    Refactored classes Review, Profile, ChangeUserDataUI, ChangeUserDataController, ReviewTest and ProfileTest and methods within the classes to english.

---
+++ core/src/main/java/cdioil/application/authz/ChangeUserDataController.java
@@ -0,0 +1,43 @@
+package cdioil.application.authz;
+
+import cdioil.domain.authz.SystemUser;
+import cdioil.persistence.impl.UserRepositoryImpl;
+import cdioil.persistence.RepositorioBaseJPA;
+
+/**
+ * Controller of US Change User's Data
+ *
+ * @author João
+ */
+public class ChangeUserDataController {
+
+    /**
+     * Logged-in user
+     */
+    private SystemUser su;
+
+    /**
+     * Creates an instance of ChangeUserDataController
+     *
+     * @param su Logged-in user
+     */
+    public ChangeUserDataController(SystemUser su) {
+        this.su = su;
+    }
+
+    /**
+     * Changes user data
+     *
+     * @param newData new data
+     * @param option int that refers to the data field to be changed
+     * @return true if the data was changed successfully, false if not
+     */
+    public boolean changeDataField(String newData, int option) {
+        boolean b = su.alterarCampoInformacao(newData, option);
+        if (b) {
+            RepositorioBaseJPA repo = new UserRepositoryImpl();
+            repo.merge(su);
+        }
+        return b;
+    }
+}
