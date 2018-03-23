60f7fc09545d182a94f04ab0c37a36f1e9279335
Ana Margarida Tavares Guerra <1161191@isep.ipp.pt>
2018-03-23 10:18:51 +0000

    Refractor of the class EAN and its Tests [UnitTest][S2] issue #12.

--- core/src/test/java/cdioil/domain/EANTest.java
+++ core/src/test/java/cdioil/domain/EANTest.java
@@ -10,43 +10,43 @@ import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
 
 /**
- * Teste da classe EAN.
+ * Test the class EAN.
  *
  * @author Ana Guerra (1161191)
  */
 public class EANTest {
 
     /**
-     * Instância de EAN para testes.
+     * Intance the EAN for tests.
      */
     EAN ean = new EAN("1700034560");
 
     /**
-     * Teste do método toString, da classe EAN.
+     * Test the method toString, of class EAN.
      */
     @Test
     public void testToString() {
         System.out.println("toString");
         
-        EAN copia = new EAN("1700034560");
+        EAN copy = new EAN("1700034560");
 
-        assertEquals("Os identificadores são iguais", copia.toString(), ean.toString());
+        assertEquals("Os identificadores são iguais", copy.toString(), ean.toString());
     }
 
     /**
-     * Teste do método hashCode, da classe EAN.
+     * Test the method hashCode, of class EAN.
      */
     @Test
     public void testHashCode() {
         System.out.println("hashCode");
 
-        EAN copia = new EAN("1700034560");
+        EAN copy = new EAN("1700034560");
 
-        assertEquals("Hash codes iguais", copia.hashCode(), ean.hashCode());
+        assertEquals("Hash codes iguais", copy.hashCode(), ean.hashCode());
     }
 
     /**
-     * Teste do método equals, da classe EAN.
+     * Test the method equals, of class EAN.
      */
     @Test
     public void testEquals() {
