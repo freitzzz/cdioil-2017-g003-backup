15f8912ded06d32800ab188b528e2a8248780d97
João Pedro Magalhães Freitas <1160907@isep.ipp.pt>
2018-03-25 16:56:47 +0100

    Added batch script file running backoffice in console Updated repository Readme

---
+++ run-backoffice-console.bat
@@ -0,0 +1,9 @@
+@Echo OFF
+REM Backoffice Console batch script based on ECAFETERIA APP made by EAPLI professors
+REM set the class path,
+
+REM assumes the build was executed with maven copy-dependencies
+SET BACKOFFICE_CONSOLE=backoffice\target\backoffice 1.0-SNAPSHOT.jar;backoffice\target\dependency\*;
+
+REM call the java VM, e.g, 
+java -cp %BACKOFFICE_CONSOLE% cdioil.backoffice.console.presentation.BackofficeMain
