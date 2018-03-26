e08deb67c613f519095e2635ac02db13cadd8e10
António Sousa [1161371] <1161371@isep.ipp.pt>
2018-03-26 17:21:13 +0100

    Add findByUserID method to both Admin and Manager's repositories.

--- core/src/main/java/cdioil/persistence/ManagerRepository.java
+++ core/src/main/java/cdioil/persistence/ManagerRepository.java
@@ -1,7 +1,20 @@
 package cdioil.persistence;
 
+import cdioil.domain.authz.Manager;
+
 /**
  * Interface for the Manager repository
+ *
  * @author <a href="1160907@isep.ipp.pt">João Freitas</a>
  */
-public interface ManagerRepository {}
+public interface ManagerRepository {
+
+    /**
+     * Retrieves an instance of Manager with the respective SystemUser database
+     * id.
+     *
+     * @param dataBaseId SystemUser database id
+     * @return Manager with the matching SystemUser id.
+     */
+    Manager findByUserID(long dataBaseId);
+}
