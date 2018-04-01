d5ec1a246805f7199f73a6db0123f029652f17a4
António Sousa [1161371] <1161371@isep.ipp.pt>
2018-04-02 00:33:30 +0100

    Add BackOfficeProperties Interface for defining the backoffice's properties file.

---
+++ backoffice/src/main/java/cdioil/backoffice/utils/BackOfficeProperties.java
@@ -0,0 +1,15 @@
+package cdioil.backoffice.utils;
+
+import java.io.File;
+
+/**
+ *
+ * @author Antonio Sousa
+ */
+public interface BackOfficeProperties {
+
+    /**
+     * BackOffice's Properties File.
+     */
+    public final static File PROPERTIESFILE = new File("src/main/resources/backoffice.properties");
+}
