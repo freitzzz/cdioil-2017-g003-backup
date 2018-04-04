5ccdee62f3af58d93ef6d4435f4a93f30c22b175
Ana Margarida Tavares Guerra <1161191@isep.ipp.pt>
2018-04-04 20:40:56 +0100

    Corrections in the class ImportQuetsionsUI [US210][Backoffice][EAPLI][S2]

--- backoffice/src/main/java/cdioil/backoffice/console/presentation/ImportQuestionsUI.java
+++ backoffice/src/main/java/cdioil/backoffice/console/presentation/ImportQuestionsUI.java
@@ -7,117 +7,81 @@ package cdioil.backoffice.console.presentation;
 
 import cdioil.application.ImportQuestionsController;
 import cdioil.backoffice.utils.Console;
-import cdioil.domain.Question;
-import java.util.List;
 
 /**
- * User Interface relativa ao caso de uso Importar Questoes através 
- * de ficheiros (US-210)
+ * User Interface for the User Story 210 - Import Questions from a File.
+ *
  * @author Ana Guerra (1161191)
  */
 public class ImportQuestionsUI {
+
     /**
-     * Constante que representa o codigo de saida da User Interface
+     * Represents the exit code for the User Interface.
      */
-    private static final String EXIT_CODE="Sair";
+    private static final String EXIT_CODE = "Sair";
     /**
-     * Constante que representa o codigo que permite saber se deve persistir 
-     * as questões importadas na base de dados
+     * Represents a message that indicates the user to insert the path of the file to import.
      */
-    private static final String PERSISTIR_BASE_DADOS_CODE="Sim";
-    /**
-     * Constante que representa a mensagem que pede o caminho para o ficheiro a ser importado
-     */
-    private static final String MENSAGEM_CAMINHO_FICHEIRO="Por favor indique o caminho do "
+    private static final String PATH_FILE_MESSAGE = "Por favor indique o caminho do "
             + "ficheiro que pretende importar";
-     /**
-     * Constante que representa a mensagem de erro a quando o caminho do ficheiro 
-     * não é encontrado
+    /**
+     * Represents a message that informs the user that the inserted filepath was not found.
      */
-    private static final String MENSAGEM_CAMINHO_FICHEIRO_NAO_ENCONTRADO="Caminho de ficheiro "
-            + "não encontrado!\nPretendo continuar? Digite \""+EXIT_CODE+"\" para sair "
+    private static final String FILEPATH_NOT_FOUND_MESSAGE = "Caminho de ficheiro "
+            + "não encontrado!\nPretendo continuar? Digite \"" + EXIT_CODE + "\" para sair "
             + "ou qualquer outra mensagem para continuar";
     /**
-     * Constante que representa a mensagem quando nenhuma questão foi importada
+     * Represents a message that informs the user that the format of the file is not valid.
      */
-    private static final String MENSAGEM_NENHUMA_QUESTAO_IMPORTADO="Nenhuma questão válido foi importada!";
+    private static final String INVALID_FORMAT_MESSAGE = "Nenhuma questão válida foi importada!";
     /**
-     * Constante que representa as mensagens quando as questões são importadas com succeso
+     * Represents a message that delimitates the imported questions.
      */
-    private static final String[] MENSAGEM_QUESTOES_IMPORTADAS={"#####Questões Importadas#####"
-            ,"#####                       #####"};
-    
+    private static final String IMPORTED_QUESTIONS_MESSAGE = "#####Questões Importadas#####";
+
     /**
-     * Constante que representa a mensagem quando a persistencia das questões na base de dados
+     * Represents a message that indicates the user to enter the exit code in order to exit.
      */
-    private static final String MENSAGEM_PERSISTIR_BASE_DADOS="Pretende guardar o novo template importados na base de dados?"
-                + "\nDigite \""+PERSISTIR_BASE_DADOS_CODE+"\" caso pretenda guardar ou qualquer outra mensagem para não guardar";
+    private static final String EXIT_MESSAGE = "A qualquer momento digite \"" + EXIT_CODE + "\" para sair";
     /**
-     * Constante que representa a mensagem quando a persistencia das questoes na base de dados 
-     * tenha ocurrido com succeso
-     */
-    private static final String MENSAGEM_PERSISTENCIA_SUCESSO="O template foi persistidos com sucesso!";
-    /**
-     * Constante que representa a mensagem a quando a persistencia das questões  na base de dados 
-     * não tenha ocurrido com succeso
-     */
-    private static final String MENSAGEM_PERSISTENCIA_FALHA="Ocorreu um erro a quando a persistencia das questões";
-    /**
-     * Constante que representa a mensagem de saida
-     */
-    private static final String MENSAGEM_SAIDA="A qualquer momento digite \""+EXIT_CODE+"\" para sair";
-    /**
-     * Controlador que controla a importação das questões atravé de ficheiros
+     * Instance of Controller that intermediates the interactions between the manager and the system.
      */
     private final ImportQuestionsController iQueCtrl;
+
     /**
-     * Constroi uma nova user interface que permite a interação entre o administrador 
-     * e a importação de utilizadores através de ficheiros
+     * Creates a new User Interface.
      */
-    public ImportQuestionsUI(){
-        iQueCtrl=new ImportQuestionsController();
-        importarQuestoes();
+    public ImportQuestionsUI() {
+        iQueCtrl = new ImportQuestionsController();
+        importQuestions();
     }
+
     /**
-     * Método que trata da interação entre o gestor e a importação das questões
+     * Method that intermediates the interactions with the manager (creates the UI itself).
      */
-    private void importarQuestoes(){
-        System.out.println(MENSAGEM_SAIDA);
-        boolean catched=false;
-        while(!catched){
-            String pathFicheiro=Console.readLine(MENSAGEM_CAMINHO_FICHEIRO);
-            if(pathFicheiro.equalsIgnoreCase(EXIT_CODE))return;
-            List<Question> listaQuestoes=iQueCtrl.lerFicheiro(pathFicheiro);
-            if(listaQuestoes==null){
-                String decisao=Console.readLine(MENSAGEM_CAMINHO_FICHEIRO_NAO_ENCONTRADO);
-                if(decisao.equalsIgnoreCase(EXIT_CODE))return;
-            }else{
-                if(listaQuestoes.isEmpty()){
-                    System.out.println(MENSAGEM_NENHUMA_QUESTAO_IMPORTADO);
-                }else{
-                    System.out.println(MENSAGEM_QUESTOES_IMPORTADAS[0]);
-                    listaQuestoes.forEach((t)->{
-                        System.out.println(t);
-                    });
-                    System.out.println(MENSAGEM_QUESTOES_IMPORTADAS[1]);
-                    catched=true;
+    private void importQuestions() {
+        System.out.println(EXIT_MESSAGE);
+        boolean catched = false;
+        while (!catched) {
+            String pathFicheiro = Console.readLine(PATH_FILE_MESSAGE);
+            if (pathFicheiro.equalsIgnoreCase(EXIT_CODE)) {
+                return;
+            }
+            int cql = iQueCtrl.lerFicheiro(pathFicheiro);
+            if (cql == 1) {
+                String decisao = Console.readLine(FILEPATH_NOT_FOUND_MESSAGE);
+                if (decisao.equalsIgnoreCase(EXIT_CODE)) {
+                    return;
                 }
             }
+            if (cql == 2) {
+                System.out.println(INVALID_FORMAT_MESSAGE);
+            }
+            if(cql == 3){
+                System.out.println(IMPORTED_QUESTIONS_MESSAGE);
+                catched = true;
+            }
+
         }
-        persistirQuestoes();
     }
-    /**
-     * Método que trata da persistencia do template na base de dados
-     */
-    private void persistirQuestoes(){
-//        String persistir=Console.readLine(MENSAGEM_PERSISTIR_BASE_DADOS);
-//        if(persistir.equalsIgnoreCase(PERSISTIR_BASE_DADOS_CODE)){
-//            if(iQueCtrl.gravarTemplate()){
-//                System.out.println(MENSAGEM_PERSISTENCIA_SUCESSO);
-//            }else{
-//                System.out.println(MENSAGEM_PERSISTENCIA_FALHA);
-//            }
-//        }
-    }
-    
 }
