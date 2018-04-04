89b80bcc1999be3a3b2e99508ff1d2461396e395
Ana Margarida Tavares Guerra <1161191@isep.ipp.pt>
2018-04-04 20:41:11 +0100

    Corrections in the class ImportQuetsionsController [US210][Backoffice][EAPLI][S2]

--- core/src/main/java/cdioil/application/ImportQuestionsController.java
+++ core/src/main/java/cdioil/application/ImportQuestionsController.java
@@ -7,8 +7,8 @@ package cdioil.application;
 
 import cdioil.application.utils.QuestoesReader;
 import cdioil.application.utils.QuestoesReaderFactory;
-import cdioil.domain.Question;
-import java.util.List;
+import cdioil.domain.GlobalLibrary;
+import cdioil.persistence.impl.GlobalLibraryRepositoryImpl;
 
 /**
  * Controlador relativo ao caso de uso Importar Questoes através de ficheiros (US-210).
@@ -17,16 +17,21 @@ import java.util.List;
  */
 public class ImportQuestionsController {
 
+    
+    private GlobalLibrary globalLibrary = new GlobalLibraryRepositoryImpl().findByQuery();
     /**
      * Importa uma lista de questoes de um ficheiro
      *
      * @param filename Caminho do ficheiro em questão
      * @return a lista de questoes lidas. Null se o ficheiro for inválido
      */
-    public List<Question> lerFicheiro(String filename) {
-        QuestoesReader questoesReader = QuestoesReaderFactory.create(filename);
-
-        return questoesReader != null ? questoesReader.readQuestoes() : null;
+    public int lerFicheiro(String filename) {
+        QuestoesReader questionsReader = QuestoesReaderFactory.create(filename);
+        if (questionsReader != null){
+            int cql = questionsReader.readQuestoes(globalLibrary);
+            if (new GlobalLibraryRepositoryImpl().merge(globalLibrary) != null) return cql ;
+        }
+        return 1;
     }
 
 }
