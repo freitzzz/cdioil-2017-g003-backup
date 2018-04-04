b7d6b80ca043986f7a8d9c36440c438d1ad03e5a
Pedro Portela(1150782) <1150782@isep.ipp.pt>
2018-04-04 23:00:48 +0100

    Small tweaks to LoginUI

--- backoffice/src/main/java/cdioil/backoffice/webapp/authz/LoginView.java
+++ backoffice/src/main/java/cdioil/backoffice/webapp/authz/LoginView.java
@@ -2,6 +2,7 @@ package cdioil.backoffice.webapp.authz;
 
 import cdioil.backoffice.webapp.admin.AdminPanelView;
 import cdioil.backoffice.webapp.manager.ManagerPanelView;
+import cdioil.backoffice.webapp.utils.ImageUtils;
 import cdioil.backoffice.webapp.utils.PopupNotification;
 import cdioil.domain.authz.Admin;
 import cdioil.domain.authz.Email;
@@ -17,43 +18,44 @@ import com.vaadin.ui.UI;
 
 /**
  * Login View class that extends LoginDesign class designed with Vaadin Design
+ *
  * @author <a href="1160907@isep.ipp.pt">João Freitas</a>
  */
-public class LoginView extends LoginDesign implements View{
+public class LoginView extends LoginDesign implements View {
     /**
      * Constant that represents the current page view name
      */
-    public static final String VIEW_NAME="LoginUI";
+    public static final String VIEW_NAME = "LoginUI";
     /**
-     * Constant that represents the name of the error dialog that pops up if the 
+     * Constant that represents the name of the error dialog that pops up if the
      * user tries to login with an invalid username
      */
-    private static final String INVALID_USERNAME_NAME="Email Inválido!";
+    private static final String INVALID_USERNAME_NAME = "Email Inválido!";
     /**
-     * Constant that represents the message that pops up if the user 
+     * Constant that represents the message that pops up if the user
      * tries to login with an invalid username
      */
-    private static final String INVALID_USERNAME_DESCRIPTION="Por favor insira um email válido";
+    private static final String INVALID_USERNAME_DESCRIPTION = "Por favor insira um email válido";
     /**
-     * Constant that represents the name of the error dialog that pops up if the 
+     * Constant that represents the name of the error dialog that pops up if the
      * user tries to login with invalid credentials
      */
-    private static final String INVALID_LOGIN_CREDENTIALS_NAME="Credenciais Erradas!";
+    private static final String INVALID_LOGIN_CREDENTIALS_NAME = "Credenciais Erradas!";
     /**
-     * Constant that represents the message that pops up if the user 
+     * Constant that represents the message that pops up if the user
      * tries to login with invalid credentials
      */
-    private static final String INVALID_LOGIN_CREDENTIALS_DESCRIPTION="As credenciais do login estão erradas";
+    private static final String INVALID_LOGIN_CREDENTIALS_DESCRIPTION = "As credenciais do login estão erradas";
     /**
-     * Constant that represents the name of the error dialog that pops up if the 
+     * Constant that represents the name of the error dialog that pops up if the
      * user tries to login with an invalid user for backoffice
      */
-    private static final String INVALID_USER_LOGIN_NAME="Acesso Invalido!";
+    private static final String INVALID_USER_LOGIN_NAME = "Acesso Invalido!";
     /**
-     * Constant that represents the message that pops up if the user 
+     * Constant that represents the message that pops up if the user
      * tries to login with an invalid user for backoffice
      */
-    private static final String INVALID_USER_LOGIN_DESCRIPTION="Não tem permissões para aceder à plataforma";
+    private static final String INVALID_USER_LOGIN_DESCRIPTION = "Não tem permissões para aceder à plataforma";
     /**
      * Current Navigator
      */
@@ -66,83 +68,103 @@ public class LoginView extends LoginDesign implements View{
      * Manager that just logged in
      */
     private Manager manager;
+
     /**
      * Creates a new LoginView
      */
-    public LoginView(){
-        navigator= UI.getCurrent().getNavigator();
+    public LoginView() {
+        navigator = UI.getCurrent().getNavigator();
+        setLogo();
         configurateLogin();
     }
+
     /**
      * Configuares login
      */
-    private void configurateLogin(){configurateLoginButton();}
+    private void configurateLogin() {
+        configurateLoginButton();
+    }
+
+    /**
+     * Gets logo from resource
+     */
+    private void setLogo() {
+        logo.setSource(ImageUtils.imagePathAsResource("/WEB-INF/logos/Feedback_Monkey_Full.png"));
+    }
+
     /**
      * Configurates login button
      */
-    private void configurateLoginButton(){
-        btnLogin.addClickListener((event)->{
-            if(checkForUsername()){
-                if(checkForLoginCredentials()){
-                    if(admin!=null){
+    private void configurateLoginButton() {
+        btnLogin.addClickListener((event) -> {
+            if (checkForUsername()) {
+                if (checkForLoginCredentials()) {
+                    if (admin != null) {
                         navigator.navigateTo(AdminPanelView.VIEW_NAME);
-                    }else if(manager!=null){
+                    } else if (manager != null) {
                         navigator.navigateTo(ManagerPanelView.VIEW_NAME);
-                    }else{
+                    } else {
                         showInvalidUserNotification();
                     }
-                    
-                }else{
+
+                } else {
                     showInvalidLoginCredentialsNotification();
                 }
-            }else{
+            } else {
                 showInvalidUsernameNotification();
             }
         });
     }
+
     /**
      * Checks if the username that the user is trying to login is valid
+     *
      * @return boolean true if the username is valid for login, false if not
      */
-    private boolean checkForUsername(){
-        try{
+    private boolean checkForUsername() {
+        try {
             new Email(txtUsername.getValue());
             return true;
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return false;
         }
     }
+
     /**
      * Checks if the user credentials are valid and if it can login in the backoffice
-     * @return Admin if the 
+     *
+     * @return Admin if the
      */
-    private boolean checkForLoginCredentials(){
-        long userID=new UserRepositoryImpl().login(new Email(txtUsername.getValue())
-                ,txtPassword.getValue());
-        if(userID==-1)return false;
-        admin=new AdminRepositoryImpl().findByUserID(userID);
-        if(admin==null)manager=new ManagerRepositoryImpl().findByUserID(userID);
+    private boolean checkForLoginCredentials() {
+        long userID = new UserRepositoryImpl().login(new Email(txtUsername.getValue())
+                , txtPassword.getValue());
+        if (userID == -1) return false;
+        admin = new AdminRepositoryImpl().findByUserID(userID);
+        if (admin == null) manager = new ManagerRepositoryImpl().findByUserID(userID);
         return true;
     }
+
     /**
      * Pops up a notification informing the user that the username is invalid
      */
-    private void showInvalidUsernameNotification(){
-        PopupNotification.show(INVALID_USERNAME_NAME,INVALID_USERNAME_DESCRIPTION
-                    ,Notification.Type.ERROR_MESSAGE,Position.TOP_RIGHT);
+    private void showInvalidUsernameNotification() {
+        PopupNotification.show(INVALID_USERNAME_NAME, INVALID_USERNAME_DESCRIPTION
+                , Notification.Type.ERROR_MESSAGE, Position.TOP_RIGHT);
     }
+
     /**
      * Pops up a notification informing the user that login credentials are invalid
      */
-    private void showInvalidLoginCredentialsNotification(){
-        PopupNotification.show(INVALID_LOGIN_CREDENTIALS_NAME,INVALID_LOGIN_CREDENTIALS_DESCRIPTION
-                    ,Notification.Type.ERROR_MESSAGE,Position.TOP_RIGHT);
+    private void showInvalidLoginCredentialsNotification() {
+        PopupNotification.show(INVALID_LOGIN_CREDENTIALS_NAME, INVALID_LOGIN_CREDENTIALS_DESCRIPTION
+                , Notification.Type.ERROR_MESSAGE, Position.TOP_RIGHT);
     }
+
     /**
      * Pops up a notification informing the user that he is not valid to access backoffice
      */
-    private void showInvalidUserNotification(){
-        PopupNotification.show(INVALID_USER_LOGIN_NAME,INVALID_USER_LOGIN_DESCRIPTION
-                    ,Notification.Type.ERROR_MESSAGE,Position.TOP_RIGHT);
+    private void showInvalidUserNotification() {
+        PopupNotification.show(INVALID_USER_LOGIN_NAME, INVALID_USER_LOGIN_DESCRIPTION
+                , Notification.Type.ERROR_MESSAGE, Position.TOP_RIGHT);
     }
 }
