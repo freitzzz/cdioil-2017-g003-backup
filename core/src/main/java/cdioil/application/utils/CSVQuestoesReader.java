6c9f3c565f3a71f5940f2b4507fe08d53112455d
Ana Margarida Tavares Guerra <1161191@isep.ipp.pt>
2018-04-04 20:41:46 +0100

    Corrections in the class CSVQuestoesReader [US210][EAPLI][S2]

--- core/src/main/java/cdioil/application/utils/CSVQuestoesReader.java
+++ core/src/main/java/cdioil/application/utils/CSVQuestoesReader.java
@@ -7,10 +7,13 @@ package cdioil.application.utils;
 
 import static cdioil.application.utils.FileReader.readFile;
 import cdioil.domain.BinaryQuestion;
-import cdioil.domain.IndependentQuestionsLibrary;
+import cdioil.domain.Category;
+import cdioil.domain.CategoryQuestionsLibrary;
+import cdioil.domain.GlobalLibrary;
 import cdioil.domain.MultipleChoiceQuestion;
 import cdioil.domain.QuantitativeQuestion;
 import cdioil.domain.Question;
+import cdioil.persistence.impl.MarketStructureRepositoryImpl;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,6 +29,10 @@ public class CSVQuestoesReader implements QuestoesReader {
      * Nome do ficheiro a ler.
      */
     private File file;
+    /**
+     * List with the Questions that were read.
+     */
+    private List<Question> lq;
 
     /**
      * Divisória entre as colunas do ficheiro.
@@ -37,9 +44,53 @@ public class CSVQuestoesReader implements QuestoesReader {
     private static final int IDENTIFIERS_LINE = 0;
 
     /**
-     * Number of identifiers (columns) in the CSV file.
+     * Max number of identifiers (columns) in the CSV file.
      */
-    private static final int NUMBER_OF_IDENTIFIERS = 9;
+    private static final int MAX_IDENTIFIERS = 9;
+    /**
+     * Min number of identifiers (columns) in the CSV file.
+     */
+    private static final int MIN_IDENTIFIERS = 8;
+    /**
+     * Hashtag Identifier.
+     */
+    private static final String HASHTAG_IDENTIFIER = "#";
+    /**
+     * DC Identifier.
+     */
+    private static final String DC_IDENTIFIER = "DC";
+    /**
+     * UN Identifier.
+     */
+    private static final String UN_IDENTIFIER = "UN";
+    /**
+     * CAT Identifier.
+     */
+    private static final String CAT_IDENTIFIER = "CAT";
+    /**
+     * SCAT Identifier.
+     */
+    private static final String SCAT_IDENTIFIER = "SCAT";
+    /**
+     * UB Identifier.
+     */
+    private static final String UB_IDENTIFIER = "UB";
+    /**
+     * Question ID Identifier.
+     */
+    private static final String ID_IDENTIFIER = "QuestaoID";
+    /**
+     * Type Identifier.
+     */
+    private static final String TYPE_IDENTIFIER = "Tipo";
+    /**
+     * Text Identifier.
+     */
+    private static final String QUESTION_IDENTIFIER = "Texto";
+    /**
+     * Scale of the evaluation
+     */
+    private static final double SCALE = 0.5;
 
     /**
      * Constrói uma instância de CSVCategoriasReader com o nome do ficheiro a ler.
@@ -48,18 +99,15 @@ public class CSVQuestoesReader implements QuestoesReader {
      */
     public CSVQuestoesReader(String filename) {
         this.file = new File(filename);
+        lq = new LinkedList<>();
     }
 
     @Override
-    public List<Question> readQuestoes() {
+    public int readQuestoes(GlobalLibrary globalLibrary) {
         List<String> fileContent = readFile(file);
         if (!isFileValid(fileContent)) {
-            return null;
+            return 2;
         }
-        List<Question> questions = new LinkedList<>();
-
-        IndependentQuestionsLibrary iqb = new IndependentQuestionsLibrary();
-
         for (int i = IDENTIFIERS_LINE + 1; i < fileContent.size(); i++) {
             String[] line = fileContent.get(i).split(SPLITTER);
             if (line.length != 0) { //Doesn't read empty lines
@@ -70,44 +118,64 @@ public class CSVQuestoesReader implements QuestoesReader {
                     String CAT = line[2];
                     String SCAT = line[3];
                     String UB = line[4];
+                    String path;
+                    if (UB.equals("")) {
+                        path = DC + DC_IDENTIFIER + "-" + UN + UN_IDENTIFIER+ "-" + CAT + CAT_IDENTIFIER +
+                                "-" + SCAT + SCAT_IDENTIFIER;
+                    } else if (SCAT.equals("") && UB.equals("") ) {
+                        path = DC + DC_IDENTIFIER + "-" + UN + UN_IDENTIFIER+ "-" + CAT + CAT_IDENTIFIER;
+                    } else if (CAT.equals("") && SCAT.equals("") && UB.equals("")) {
+                        path = DC + DC_IDENTIFIER + "-" + UN + UN_IDENTIFIER;
+                    } else if (UN.equals("") && CAT.equals("") && SCAT.equals("") && UB.equals("")) {
+                        path = DC + DC_IDENTIFIER;
+                    } else {
+                        path = DC + DC_IDENTIFIER + "-" + UN + UN_IDENTIFIER+ "-" + CAT + CAT_IDENTIFIER +
+                                "-" + SCAT + SCAT_IDENTIFIER + "-" + UB + UB_IDENTIFIER;
+                    }
+                    System.out.println("PATH:   " + path);
                     String ID = line[5];
                     String tipo = line[6];
 
-                    String param = line[8];
-                    Question question;
+                    Category cat = new MarketStructureRepositoryImpl().
+                            findCategoriesByPathPattern(path.toUpperCase());
+                    if (cat == null) {
+                        return 2;
+                    }
+
+                    globalLibrary.getCatQuestionsLibrary().addCategory(cat);
                     if (tipo.equalsIgnoreCase("SN")) {
-                        question = new BinaryQuestion(line[7],ID);
+                        Question question = new BinaryQuestion(line[7], ID);
+                        addQuestion(globalLibrary.getCatQuestionsLibrary(), question, cat);
+
                     } else if (tipo.equalsIgnoreCase("EM")) {
+                        String param = line[8];
                         String[] pEM = param.split("=");
                         int nrEM = Integer.parseInt(pEM[1]);
                         LinkedList<String> options = new LinkedList<>();
                         for (int a = 0; a < nrEM; a++) {
-                            options.add(a + 1 + "");
+                            line = fileContent.get(i + 1).split(SPLITTER);
+                            i++;
+                            options.add(line[7]);
                         }
-                        question = new MultipleChoiceQuestion(line[7],ID, options);
+                        Question question = new MultipleChoiceQuestion(line[7], ID, options);
+                        addQuestion(globalLibrary.getCatQuestionsLibrary(), question, cat);
+
                     } else if (tipo.equalsIgnoreCase("ESC")) {
-                        String[] pESC = param.split(";");
-                        String[] pMin = pESC[0].split("=");
-                        String[] pMax = pESC[1].split("=");
+                        String[] pMin = line[8].trim().split("=");
+                        String[] pMax = line[9].trim().split("=");
+                        pMax[1] = pMax[1].replace('"', ' ').trim();
                         double min = Double.parseDouble(pMin[1]);
                         double max = Double.parseDouble(pMax[1]);
-                        question = new QuantitativeQuestion(line[7], ID,min, max, 0.5);
-                    }//else{
-//                        ERRRORRRR
-//                    }
-//                    boolean added = iqb.addQuestion(question);
-//                    if (added) {
-//                        questions.add(question);
-//                    }
+                        Question question = new QuantitativeQuestion(line[7], ID, min, max, SCALE);
+                        addQuestion(globalLibrary.getCatQuestionsLibrary(), question, cat);
 
+                    }
                 } catch (IllegalArgumentException ex) {
-                    System.out.println("O formato das Questôes inválido na linha " + i + ".");
-
+                    System.out.println("O formato das Questões é inválido na linha " + i + ".");
                 }
             }
-
         }
-        return questions;
+        return 3;
     }
 
     /**
@@ -120,7 +188,29 @@ public class CSVQuestoesReader implements QuestoesReader {
         if (fileContent == null) {
             return false;
         }
-        return fileContent.get(IDENTIFIERS_LINE).split(SPLITTER).length == NUMBER_OF_IDENTIFIERS;
+        String[] line = fileContent.get(IDENTIFIERS_LINE).split(SPLITTER);
+
+        return line.length == MIN_IDENTIFIERS || line.length == MAX_IDENTIFIERS;
+//                 && line[0].equalsIgnoreCase(HASHTAG_IDENTIFIER + DC_IDENTIFIER)
+//                && line[1].equalsIgnoreCase(HASHTAG_IDENTIFIER + UN_IDENTIFIER)
+//                && line[2].equalsIgnoreCase(HASHTAG_IDENTIFIER + CAT_IDENTIFIER)
+//                && line[3].equalsIgnoreCase(HASHTAG_IDENTIFIER + SCAT_IDENTIFIER)
+//                && line[4].equalsIgnoreCase(HASHTAG_IDENTIFIER + UB_IDENTIFIER)
+//                && line[5].equalsIgnoreCase(ID_IDENTIFIER)
+//                && line[6].equalsIgnoreCase(TYPE_IDENTIFIER)
+//                && line[7].equalsIgnoreCase(QUESTION_IDENTIFIER);
+    }
+
+    /**
+     * Adds a question to the Category Questions Library.
+     *
+     * @param cqb CategoryQuestionsLibrary
+     * @param que Question
+     * @param cat Category
+     * @return true if the question is successfully added or false if not.
+     */
+    public boolean addQuestion(CategoryQuestionsLibrary cqb, Question que, Category cat) {
+        return cqb.addQuestion(que, cat);
     }
 
 }
