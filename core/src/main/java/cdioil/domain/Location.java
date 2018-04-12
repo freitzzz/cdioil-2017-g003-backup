de85db72f851de213b4d4000cc8006d9cdb8d342
João Pedro Magalhães Freitas <1160907@isep.ipp.pt>
2018-04-12 23:02:25 +0100

    Added Location class that represents the location of a certain SystemUser
    see #21

---
+++ core/src/main/java/cdioil/domain/Location.java
@@ -0,0 +1,83 @@
+package cdioil.domain;
+
+import cdioil.framework.domain.ddd.ValueObject;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Location class that represents a certain location
+ * <br>Value Object of SystemUser
+ * @author <a href="1160907@isep.ipp.pt">João Freitas</a>
+ */
+public class Location implements ValueObject,Serializable{
+    /**
+     * Constant that represents an empty String
+     */
+    private static final String EMPTY_STRING="";
+    /**
+     * Constant that represents the regular expression used to identify all 
+     * possible spaces
+     */
+    private static final String REGEX_ALL_SPACES="\\s+";
+    /**
+     * Constant that represents the message of an invalid location
+     */
+    private static final String INVALID_LOCATION_MESSAGE="A localidade é inválida!";
+    /**
+     * String that represents the location of a certain SystemUser
+     */
+    private String location;
+    /**
+     * Builds a new Location that represents the location of a SystemUser
+     * @param location String with the SystemUser location
+     */
+    public Location(String location){
+        checkLocation(location);
+        this.location=location;
+    }
+    /**
+     * Method that checks if a certain Location is equal to the current Location
+     * @param obj Location with the location being compared with the current one
+     * @return boolean true if both locations are equal, false if not
+     */
+    @Override
+    public boolean equals(Object obj){
+        if(obj==this)return true;
+        if(obj==null||obj.getClass()!=this.getClass())return false;
+        return location.equalsIgnoreCase(((Location)obj).location);
+    }
+    /**
+     * Location Hashcode
+     * @return Integer with the current Location hashcode
+     */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.location);
+        return hash;
+    }
+    /**
+     * Method that represents the textual information of a Location
+     * @return String with the textual representation of the current Location
+     */
+    @Override
+    public String toString(){
+        return location;
+    }
+    /**
+     * Method that checks if a certain location is valid or not
+     * <br>Throws an <b>IllegalArgumentException</b> if the location is invalid
+     * <br><b>Warning</b>: Currently not regexing all possible Locations, due to 
+     * application localization
+     * @param location String with the location being validated
+     */
+    private void checkLocation(String location){
+        if(location==null||location.replaceAll(REGEX_ALL_SPACES,EMPTY_STRING).isEmpty()){
+            throw new IllegalArgumentException(INVALID_LOCATION_MESSAGE);
+        }
+    }
+    /**
+     * Protected constructor in order to allow JPA persistence
+     */
+    protected Location(){}
+}
---
+++ core/src/test/java/cdioil/domain/LocationTest.java
@@ -0,0 +1,87 @@
+package cdioil.domain;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests relatively to Location class
+ * @author <a href="1160907@isep.ipp.pt">João Freitas</a>
+ */
+public class LocationTest {
+    
+    public LocationTest() {
+    }
+    
+    /**
+     * Test of Location constructor
+     */
+    @Test
+    public void testConstructor(){
+        assertNull("The condition should be successful since the location being created is null"
+                ,createLocation(null));
+        assertNull("The condition should be successful since the location being created is empty"
+                ,createLocation(""));
+        assertNull("The condition should be successful since the location being created is null"
+                ,createLocation("                    "));
+        assertNotNull("The condition should be successful since the location being created is valid"
+                ,createLocation("Porto"));
+    }
+    
+    /**
+     * Test of equals method, of class Location.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        Location locationX=createLocation("Porto");
+        Location locationY=createLocation("Lisboa");
+        Location locationZ=createLocation("PORTO");
+        assertEquals("The condition should be successful since the Location being compared "
+                + "is the same instance",locationX,locationX);
+        assertNotEquals("The condition should be successful since the Location being "
+                + "compared is null",locationX,null);
+        assertNotEquals("The condition should be successful since the Location being "
+                + "compared is from another class",locationX,"");
+        assertNotEquals("The condition should be successful since the locations being "
+                + "are not the same",locationX,locationY);
+        assertEquals("The condition should be successful since the locations being "
+                + "are the same",locationX,locationZ);
+        
+    }
+
+    /**
+     * Test of hashCode method, of class Location.
+     */
+    @Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+        Location locationX=createLocation("Porto");
+        Location locationY=createLocation("Porto");
+        assertEquals("The condition should be successful since both locations hashcode "
+                + "are the same",locationX.hashCode(),locationY.hashCode());
+    }
+
+    /**
+     * Test of toString method, of class Location.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        String location="Lil Pump";
+        assertEquals("The condition should be successful since the content of the Strings "
+                + "are the same",location,createLocation(location).toString());
+    }
+    /**
+     * Miscellaneous tests
+     */
+    @Test
+    public void testMisc(){
+        assertNotNull(new Location());
+    }
+    /**
+     * Method that creates a new Location
+     * @param location String with the location being created
+     * @return Location with the created location
+     */
+    private Location createLocation(String location){try{return new Location(location);}catch(IllegalArgumentException e){ return null;}}
+}
