1f9cd1a2dd34e541725e05034b960b656e8bf8e8
João Moreira <1160928@isep.ipp.pt>
2018-04-18 12:59:12 +0100

    Changes to classes TargetedSurvey, SurveyReposutoryImpl and AddUsersQuestionnaireController, as well as changes to SurveyRepository interface and TargetedSurveyTest unit tests.

--- core/src/main/java/cdioil/persistence/SurveyRepository.java
+++ core/src/main/java/cdioil/persistence/SurveyRepository.java
@@ -1,7 +1,19 @@
 package cdioil.persistence;
 
+import cdioil.domain.TargetedSurvey;
+import java.util.List;
+
 /**
  * Interface for the Survey Repository
+ *
  * @author <a href="1160907@isep.ipp.pt">João Freitas</a>
  */
-public interface SurveyRepository {}
+public interface SurveyRepository {
+
+    /**
+     * Returns all Targeted Surveys
+     *
+     * @return list of targeted surveys
+     */
+    public abstract List<TargetedSurvey> getTargetedSurveys();
+}
