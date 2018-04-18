a79fd7331693c9744ba574dc25c018133cab0746
Ana Margarida Tavares Guerra <1161191@isep.ipp.pt>
2018-04-18 23:49:33 +0100

    Corrections in the US203 according the CSV file. [US203] [EAPLI] [S3]

--- backoffice/src/main/java/cdioil/backoffice/application/ImportProductsController.java
+++ backoffice/src/main/java/cdioil/backoffice/application/ImportProductsController.java
@@ -8,6 +8,7 @@ package cdioil.backoffice.application;
 import cdioil.application.utils.ProductsReader;
 import cdioil.application.utils.ProductsReaderFactory;
 import cdioil.domain.Category;
+import cdioil.domain.MarketStructure;
 import cdioil.domain.Product;
 import cdioil.files.InvalidFileFormattingException;
 import cdioil.persistence.impl.MarketStructureRepositoryImpl;
@@ -16,7 +17,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.HashMap;
 
 /**
  * Controller for use cases US-203 (import products from file).
@@ -24,19 +24,6 @@ import java.util.HashMap;
  * @author Ana Guerra (1161191)
  */
 public class ImportProductsController {
-
-    /**
-     * Intance of ProductRepositoryImpl
-     */
-    private final ProductRepositoryImpl productRepositoryImpl;
-
-    /**
-     * Instantiates a controller for importing products.
-     */
-    public ImportProductsController() {
-        this.productRepositoryImpl = new ProductRepositoryImpl();
-    }
-
     /**
      * Import products from a file.
      *
@@ -47,37 +34,44 @@ public class ImportProductsController {
     public Integer importProducts(String fileName) throws InvalidFileFormattingException {
 
         Set<Product> successfullyImportedProducts = new HashSet<>();
-
+    
         ProductsReader productsReader = ProductsReaderFactory.create(fileName);
         if (productsReader != null) {
 
-        }
+        
         Map<String, List<Product>> productByCatPath = productsReader.readProducts();
-        Map<String, Product> produtsImported = new HashMap<>();
+
 
         MarketStructureRepositoryImpl marketStructureRepository = new MarketStructureRepositoryImpl();
+        MarketStructure marketStructure = marketStructureRepository.findMarketStructure();
 
         Set<Map.Entry<String, List<Product>>> entries = productByCatPath.entrySet();
 
-        for(Map.Entry<String, List<Product>> mapEntry : entries) {
-
+        for (Map.Entry<String, List<Product>> mapEntry : entries) {
             String path = mapEntry.getKey();
             List<Product> productList = mapEntry.getValue();
 
-            List<Category> categoryList = marketStructureRepository.findCategoriesByPathPattern(path);
-
+            List<Category> categoryList = marketStructure.getAllCategories();
             if (categoryList == null) {
                 continue;
             }
-            if (categoryList.contains(path)) {
-                productList.forEach((pro) -> {
-                    successfullyImportedProducts.add(pro);
-                    produtsImported.put(path, pro);
-                });
-            }
-        }
-        productRepositoryImpl.merge(produtsImported);
+            for (Category cat : categoryList) {
+                
+                if (cat.categoryPath().equalsIgnoreCase(path)) {
+                    productList.forEach((pro) -> {
+                        successfullyImportedProducts.add(pro);
+                       marketStructure.addProduct(pro, cat);
 
+                    });
+                }//else{
+//                    System.out.println("A categoria " + path + " não existe na estrutura mercadologica");
+//                }
+            }
+
+        }
+
+//        marketStructureRepository.merge(marketStructure);
+        }
         return successfullyImportedProducts.size();
     }
 
