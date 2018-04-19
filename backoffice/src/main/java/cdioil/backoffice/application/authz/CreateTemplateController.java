1307d7302da4ab8e4ee62825c5d95cd547a18806
Joana Pinheiro [1161380] <1161380@isep.ipp.pt>
2018-04-19 10:52:30 +0100

    [see #26] Added CreateTemplateController

--- backoffice/src/main/java/cdioil/backoffice/application/authz/CreateTemplateController.java
+++ backoffice/src/main/java/cdioil/backoffice/application/authz/CreateTemplateController.java
@@ -1,4 +1,26 @@
 package cdioil.backoffice.application.authz;
 
+
+import cdioil.domain.*;
+import cdioil.domain.authz.Manager;
+import cdioil.persistence.impl.CategoryQuestionsLibraryRepositoryImpl;
+import cdioil.persistence.impl.CategoryTemplatesLibraryRepositoryImpl;
+import cdioil.persistence.impl.IndependentQuestionsLibraryRepositoryImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateTemplateController {
+
+    private CategoryQuestionsLibrary categoryQuestionsLibrary;
+    private IndependentQuestionsLibrary independentQuestionsLibrary;
+
+    public CreateTemplateController() {
+        CategoryQuestionsLibraryRepositoryImpl categoryQuestionsLibraryRepository = new CategoryQuestionsLibraryRepositoryImpl();
+        IndependentQuestionsLibraryRepositoryImpl independentQuestionsLibraryRepository = new IndependentQuestionsLibraryRepositoryImpl();
+
+        categoryQuestionsLibrary = categoryQuestionsLibraryRepository.findCategoryQuestionsLibrary();
+        independentQuestionsLibrary = independentQuestionsLibraryRepository.findLibrary();
+
+    }
 }
