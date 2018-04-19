9f77a182c552c6be1d6ca78a9922b53b264c6309
Pedro Portela(1150782) <1150782@isep.ipp.pt>
2018-04-19 18:47:49 +0100

    Adds Register User functionality to web app

--- backoffice/src/main/java/cdioil/backoffice/webapp/authz/RegisterPopupViewContent.java
+++ backoffice/src/main/java/cdioil/backoffice/webapp/authz/RegisterPopupViewContent.java
@@ -1,15 +1,20 @@
 package cdioil.backoffice.webapp.authz;
 
+import cdioil.frontoffice.application.authz.RegisterUserController;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.PopupView;
+import com.vaadin.ui.ProgressBar;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+
+import java.time.LocalDate;
 
 public class RegisterPopupViewContent implements PopupView.Content {
 
@@ -25,14 +30,16 @@ public class RegisterPopupViewContent implements PopupView.Content {
     private DateField birthdayField;
     private Button confirmButton;
 
-
+    private RegisterUserController controller;
 
     public RegisterPopupViewContent() {
         instantiateComponents();
         prepareComponent();
+        prepareConfirmButton();
     }
 
     private void instantiateComponents() {
+        controller = new RegisterUserController();
         mainLayout = new VerticalLayout();
         mainLayout.setSpacing(true);
 
@@ -62,6 +69,55 @@ public class RegisterPopupViewContent implements PopupView.Content {
 
         mainLayout.addComponentsAndExpand(confirmButton);
         mainLayout.setComponentAlignment(confirmButton, Alignment.MIDDLE_CENTER);
+    }
+
+    private void prepareConfirmButton() {
+        confirmButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                final String firstName = firstNameTextField.getValue();
+                final String lastName = lastNameTextField.getValue();
+                final String email = emailTextField.getValue();
+                final String phone = phoneTextField.getValue();
+                final String password = passwordField.getValue();
+                final String repeatPassword = repeatPasswordField.getValue();
+                final String city = cityTextField.getValue();
+                final LocalDate birthday = birthdayField.getValue();
+
+                // Checks if passwords match
+                if (!password.equals(repeatPassword)) {
+                    Notification.show("Passwords não são coincidem", Notification.Type.ERROR_MESSAGE);
+                    return;
+                }
+
+                // Check for blank fields
+                if (firstName == null
+                        || lastName == null
+                        || email == null
+                        || phone == null
+                        || password == null
+                        || repeatPassword == null
+                        || city == null
+                        || birthday == null) {
+                    Notification.show("Todos os campos são de preenchimento obrigatório",
+                            Notification.Type.ERROR_MESSAGE);
+                    return;
+                }
+
+                try {
+                    controller.registerUser(firstName, lastName, email,
+                            password, phone, city, birthday.toString());
+                } catch (IllegalArgumentException e) {
+                    Notification.show(e.getMessage(),
+                            Notification.Type.ERROR_MESSAGE);
+                    e.printStackTrace();
+                    return;
+                }
+
+                Notification.show("Utilizador Registado com sucesso!",
+                        Notification.Type.HUMANIZED_MESSAGE);
+            }
+        });
     }
 
     @Override
