9e561bf40d59a63b77ad52b44747c9b3a399295a
João Pedro Magalhães Freitas <1160907@isep.ipp.pt>
2018-05-01 21:58:35 +0100

    RCOMP Added missing reference for the hash generation on AuthKeyGenerator .C and header files Improved a bit the performance of the generator algorithm

--- RCOMP/US931/AuthKeyGenerator.c
+++ RCOMP/US931/AuthKeyGenerator.c
@@ -1,16 +1,20 @@
 /*Generator Include Header*/
 #include "AuthKeyGenerator.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <openssl/sha.h>
 
+/*Hash Buffer Size*/
 #define BUFFER_SIZE 100
+/*Mac Address and MSN concatenation format for sprintf*/
+#define MAC_MSN_FORMAT "%s\n%lu"
+/*Hash Concatenation format for sprintf*/
+#define HASH_FORMAT "%02x"
 
+/* Generates a Authentication Key based on the computer Mac Address & MSN
+ * <br>Uses SHA-512 for hash generation
+ * <br>Inspired from https://stackoverflow.com/a/2458382
+ */
 char* generate(){
     char macAndFakeMSN[BUFFER_SIZE];
-    sprintf(macAndFakeMSN,"%s\n%lu",get_mac_address(),getFakeMSN());
-    printf("%s\n",macAndFakeMSN);
+    sprintf(macAndFakeMSN,MAC_MSN_FORMAT,get_mac_address(),getFakeMSN());
     SHA512_CTX sha512;
     unsigned char hash[SHA512_DIGEST_LENGTH];
     unsigned char* realHash=malloc(SHA512_DIGEST_LENGTH);
@@ -18,12 +22,6 @@ char* generate(){
     SHA512_Update(&sha512,macAndFakeMSN,strlen(macAndFakeMSN));
     SHA512_Final(hash,&sha512);
     int i;
-    for(i=0;i<SHA512_DIGEST_LENGTH;i++)sprintf(realHash+(i*2),"%02x",hash[i]);
+    for(i=0;i<SHA512_DIGEST_LENGTH;i++)sprintf(realHash+(i<<1),HASH_FORMAT,hash[i]);
     return realHash;
-}
-
-
-int main(){
-    printf("%s\n",generate());
-    return 0;
 }
