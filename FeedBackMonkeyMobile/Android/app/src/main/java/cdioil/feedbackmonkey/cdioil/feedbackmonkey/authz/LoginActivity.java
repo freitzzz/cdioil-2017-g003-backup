90ca48ddcc664afe5b7abef26071b7ed2567011d
João Pedro Magalhães Freitas <1160907@isep.ipp.pt>
2018-05-10 16:45:54 +0100

    Added initial authentication activity package

---
+++ FeedBackMonkeyMobile/Android/app/src/main/java/cdioil/feedbackmonkey/cdioil/feedbackmonkey/authz/LoginActivity.java
@@ -0,0 +1,13 @@
+package cdioil.feedbackmonkey.cdioil.feedbackmonkey.authz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
