c19d7bc0d2ebd0769a1f4e30c9565ba5fd53c6b6
Joana Pinheiro [1161380] <1161380@isep.ipp.pt>
2018-05-15 11:06:38 +0100

    Corrected errors

--- backoffice/src/main/java/cdioil/backoffice/console/presentation/LoadAnswersUI.java
+++ backoffice/src/main/java/cdioil/backoffice/console/presentation/LoadAnswersUI.java
@@ -48,7 +48,7 @@ public class LoadAnswersUI {
             System.out.println("Finished.");
             System.out.println(numAnswers + " Reviews added");
             System.out.println(SEPARATOR);
-        } catch (InvalidFileFormattingException|InterruptedException e) {
+        } catch (InvalidFileFormattingException e) {
             System.out.println("File does not exist or has invalid formatting.");
         }
     }
