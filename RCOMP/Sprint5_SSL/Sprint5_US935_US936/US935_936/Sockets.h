5f903a1836c286db39d2d3b4532909b88ce55f7a
Gil Durão <1160936@isep.ipp.pt>
2018-05-26 13:36:18 +0100

    Implementation of USs 935 and 936 using SSL encryption fix #41 see #36

---
+++ RCOMP/Sprint5_SSL/Sprint5_US935_US936/US935_936/Sockets.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define BUF_SIZE 300
+#define SERVER_PORT "9999"
