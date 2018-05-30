f24fe45435118881c40850c0e7d3bf42204ad5ad
Joana Pinheiro [1161380] <1161380@isep.ipp.pt>
2018-05-30 16:06:33 +0100

    [see #40] Added QuantitiveQuestionActivity

---
+++ FeedBackMonkeyMobile/Android/app/src/main/java/cdioil/feedbackmonkey/authz/QuantitativeQuestionActivity.java
@@ -0,0 +1,15 @@
+package cdioil.feedbackmonkey.authz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cdioil.feedbackmonkey.R;
+
+public class QuantitativeQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quantitative_question);
+    }
+}
