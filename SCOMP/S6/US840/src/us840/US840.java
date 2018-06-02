8049948e08e154a30c5caabcecc2404417431904
Gil Durão <1160936@isep.ipp.pt>
2018-06-02 12:01:37 +0100

    Added base project for US840 - Simulação concorrente de receção de inquéritos see #44

---
+++ SCOMP/S6/US840/src/us840/US840.java
@@ -0,0 +1,18 @@
+package us840;
+
+/**
+ * Main class that runs the simulation of a server concurrently receiving
+ * reviews from several feedback devices using threads.
+ *
+ * @author Group 3 CDIO-IL 2DX
+ */
+public class US840 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
