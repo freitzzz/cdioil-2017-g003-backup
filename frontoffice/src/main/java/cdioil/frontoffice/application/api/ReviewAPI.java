10302d5dfad973bc1a86d5b4408c2f2db201505f
António Sousa [1161371] <1161371@isep.ipp.pt>
2018-06-06 16:11:53 +0100

    Refactor ReviewResource and ReviewAPI in order to allow for answering a survey when in offline mode.

--- frontoffice/src/main/java/cdioil/frontoffice/application/api/ReviewAPI.java
+++ frontoffice/src/main/java/cdioil/frontoffice/application/api/ReviewAPI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cdioil.frontoffice.application.api;
 
 import javax.ws.rs.core.Response;
@@ -16,44 +11,14 @@ import javax.ws.rs.core.Response;
 public interface ReviewAPI {
 
     /**
-     * Shows the question via JSON GET Request.
+     * Produces an HTTP Response whereby the response's body contains an XML
+     * representation of a Review.
      *
-     * @param authenticationToken Authentication token of the user
-     * @param surveyID ID of the survey in question
-     * @param reviewID ID of the review
-     * @return JSON response with the current question
+     * @param authenticationToken user's session authentication token
+     * @param surveyID database identifier of the survey being answered
+     * @return HTTP Response with an XML representation of a new Review if no
+     * errors have occured.
      */
-    public Response showQuestion(String authenticationToken, String surveyID, String reviewID);
+    public Response newReview(String authenticationToken, String surveyID);
 
-    /**
-     * Allows the user to answer a question via JSON PUT Request.
-     *
-     * @param authenticationToken Authentication token of the user
-     * @param option Chosen option
-     * @param questionType Type of question
-     * @param surveyID ID of the survey
-     * @param reviewID ID of the review
-     * @return JSON response with the next question
-     */
-    public Response answerQuestion(String authenticationToken, String option, String questionType, String surveyID, String reviewID);
-
-    /**
-     * Creates a review via JSON POST Request.
-     *
-     * @param authenticationToken Authentication token of the user
-     * @param surveyID ID of the survey
-     * @return JSON response with the database ID of the review
-     */
-    public Response createReview(String authenticationToken, String surveyID);
-
-    /**
-     * Submits a suggestion via a JSON PUT Request
-     *
-     * @param suggestion suggestion to submit
-     * @param surveyID ID of the survey
-     * @param reviewID id of the review
-     * @param authenticationToken Authentication token of the user
-     * @return JSON response
-     */
-    public Response submitSuggestion(String suggestion, String surveyID, String reviewID, String authenticationToken);
 }
