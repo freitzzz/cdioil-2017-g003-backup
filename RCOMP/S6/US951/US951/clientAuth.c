1413895f3de38069cbe6690b1474b720f574d127
João Pedro Magalhães Freitas <1160907@isep.ipp.pt>
2018-06-07 14:53:06 +0100

    Fixed corruption map on clientAuth.c (Added more bytes to product_name on Review.h)
    see #48

--- RCOMP/S6/US951/US951/clientAuth.c
+++ RCOMP/S6/US951/US951/clientAuth.c
@@ -30,7 +30,11 @@ char produtos[][20] = {
 
   int currentReviews=0;
   int currentReviewsSize=1;
+  int currentReviewsIndex=0;
+
   Review* initialize_review_shm_queue() {
+    currentReviews=0;
+    currentReviewsSize=1;
       FILE* fileStream=fopen(QUEUE_NAME,READ_RIGHTS);
       Review* reviews=malloc(sizeof(Review));
       if(fileStream==NULL){
@@ -58,45 +62,75 @@ void persist_queue_reviews(Review* queueReviews) {
       int i;
       for(i=0; i<currentReviews; i++) {
           if(queueReviews[i].id!=-1) {
-              Review reviewX;
-              strcpy(reviewX.product_name,queueReviews[i].product_name);
-              reviewX.id=queueReviews[i].id;
-              reviewX.valor=queueReviews[i].valor;
-              fwrite(&reviewX,sizeof(Review),1,fileStream);
+              fwrite(&queueReviews[i],sizeof(Review),1,fileStream);
           }
       }
       fclose(fileStream);
   }
 
+  Review* get_reviews_to_persist(Cominhos c){
+    Review review;
+  	time_t t;
 
-int send_reviews(char* ip,char* authenticationKey){
+  	srand(time(&t)); //generate seed
+  	int idx = rand() % 9;
+
+  	BYTE randomProduct[20];
+  	strcpy((char*)randomProduct, produtos[idx]); //fetch random product
+  	int id = getpid();
+  	int randomValor = rand() % 6;
+
+  	WORD key_schedule[60];
+  	BYTE iv[1][16] = {
+  		{0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f}
+  	};
+
+  	BYTE key[1][32];
+  	strncpy((char*)key, c.key, 32); //Gets first 32 bytes of authentication key for encryption key
+  	//Prepares key for encryption
+  	aes_key_setup(key[0], key_schedule, 256);
+  	//Encrypt the information
+  	aes_encrypt_ctr((BYTE*)randomProduct,20, (BYTE*)review.product_name, key_schedule, 256, iv[0]);
+  	aes_encrypt_ctr((BYTE*)&id,4, (BYTE*)&review.id, key_schedule, 256, iv[0]);
+  	aes_encrypt_ctr((BYTE*)&randomValor,4, (BYTE *)&review.valor, key_schedule, 256, iv[0]);
+    Review* reviewsX=initialize_review_shm_queue();
+    strcpy(reviewsX[currentReviews-1].product_name,review.product_name);
+    reviewsX[currentReviews-1].id=review.id;
+    reviewsX[currentReviews-1].valor=review.valor;
+    return reviewsX;
+  }
+
+
+int send_reviews(char* ip,Cominhos c,Review* reviews){
   SSL_CTX *sslctx; //SSL context structure
 	SSL *cSSL; //SSL socket
 
 	initializeSSL(); //Initializes SSL libraries
 
 
-
     struct addrinfo req, *list;
     bzero((char *)&req, sizeof(req)); //Clears the structure
     req.ai_family = AF_UNSPEC; //Defines the family of the connection
     req.ai_socktype = SOCK_STREAM; //Defines the type of the socket
-
+    printf(">>>>>>>\n");
     int failure=getaddrinfo(ip, SERVER_PORT, &req, &list); //Fills the structure with the needed information
     if(failure){
         printf("An error ocured while retrieving server info\n");
+        persist_queue_reviews(reviews);
         return 0;
     }
-
+    printf("<<<<<<<\n");
     int sock = socket(list->ai_family, list->ai_socktype, list->ai_protocol); //Creates a socket to use in the TCP connection
     if(sock==-1){
         printf("An error ocured while creating the TCP Connection Socket\n");
+        persist_queue_reviews(reviews);
         return 0;
     }
 
     int connectionStatus=connect(sock, (struct sockaddr *)list->ai_addr, list->ai_addrlen); //Creates the TCP connection
     if(connectionStatus==-1){
         printf("An error occured while connecting to the TCP Connection\n");
+        persist_queue_reviews(reviews);
         return 0;
     }
 
@@ -108,6 +142,7 @@ int send_reviews(char* ip,char* authenticationKey){
 	SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
 	if(load_cert <= 0){
 		printf("An error occured while loading the server's certificate\n");
+    persist_queue_reviews(reviews);
 		return 0;
 	}
 
@@ -115,6 +150,7 @@ int send_reviews(char* ip,char* authenticationKey){
 	int use_cert = SSL_CTX_use_certificate_file(sslctx, CERT_FILE , SSL_FILETYPE_PEM);
 	if(use_cert <= 0){
 		printf("An error occured while setting up the client's certificate file\n");
+    persist_queue_reviews(reviews);
 		return 0;
 	}
 
@@ -124,31 +160,28 @@ int send_reviews(char* ip,char* authenticationKey){
 	int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, PRIVATE_KEY_FILE, SSL_FILETYPE_PEM);
 	if(use_prv <= 0){
 		printf("An error occured while setting up the client's private key\n");
+    persist_queue_reviews(reviews);
+    return 0;
 	}
 
 	/*Make sure the key and certificate file match*/
 	if (SSL_CTX_check_private_key(sslctx) == 0) {
 	   printf("Private key does not match the certificate public key\n");
-	   exit(0);
+     persist_queue_reviews(reviews);
+	   return 0;
 	}
 
 	/* Set the list of trusted CAs based on the file and/or directory provided*/
 	if(SSL_CTX_load_verify_locations(sslctx,CERT_FILE,NULL)<1) {
 	   printf("Error setting verify location\n");
-	   exit(0);
+     persist_queue_reviews(reviews);
+	   return 0;
 	}
 
 	/* Set for server verification*/
 	SSL_CTX_set_verify(sslctx,SSL_VERIFY_PEER,NULL);
 
-    Cominhos c;
 
-	char temp[256];
-
-	strncpy(temp,authenticationKey, sizeof(temp));
-	strncpy(c.key, temp, sizeof(temp));
-
-    time(&c.timestamp);
 
 	cSSL = SSL_new(sslctx); //Creates the SSL socket with a specific context structure
 	SSL_set_fd(cSSL, sock); //Connects the SSL object with a file descriptor
@@ -157,6 +190,7 @@ int send_reviews(char* ip,char* authenticationKey){
 
 	if(ssl_err < 1){
 		printf("An error occured while performing the SSL handshake\n");
+    persist_queue_reviews(reviews);
 		return 0;
 	}
 
@@ -171,6 +205,7 @@ int send_reviews(char* ip,char* authenticationKey){
 			close(sock);
 			SSL_CTX_free(sslctx);
 		}
+    persist_queue_reviews(reviews);
 		return 0;
 	}
 
@@ -187,42 +222,22 @@ int send_reviews(char* ip,char* authenticationKey){
 			close(sock);
 			SSL_CTX_free(sslctx);
 		}
+    persist_queue_reviews(reviews);
 		return 0;
 	}
-
+  printf("%d\n",statusCode);
 	if(statusCode==FAILURE_CODE){
 		printf("The device is not allowed to send reviews\n");
 		close(sock);
+    persist_queue_reviews(reviews);
 		return 0;
 	}
 
-	Review review;
-	time_t t;
 
-	srand(time(&t)); //generate seed
-	int idx = rand() % 9;
 
-	BYTE randomProduct[20];
-	strcpy((char*)randomProduct, produtos[idx]); //fetch random product
-	int id = getpid();
-	int randomValor = rand() % 6;
-
-	WORD key_schedule[60];
-	BYTE iv[1][16] = {
-		{0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f}
-	};
-
-	BYTE key[1][32];
-	strncpy((char*)key, c.key, 32); //Gets first 32 bytes of authentication key for encryption key
-	//Prepares key for encryption
-	aes_key_setup(key[0], key_schedule, 256);
-	//Encrypt the information
-	aes_encrypt_ctr((BYTE*)randomProduct,20, (BYTE*)review.product_name, key_schedule, 256, iv[0]);
-	aes_encrypt_ctr((BYTE*)&id,4, (BYTE*)&review.id, key_schedule, 256, iv[0]);
-	aes_encrypt_ctr((BYTE*)&randomValor,4, (BYTE *)&review.valor, key_schedule, 256, iv[0]);
-
-	ssl_err = SSL_write(cSSL,&review,sizeof(Review)); //Writes the review to the socket
-	if(ssl_err <= 0){
+	ssl_err = SSL_write(cSSL,&reviews[0],sizeof(Review)); //Writes the review to the socket
+  reviews[0].id=-1;
+  if(ssl_err <= 0){
 		printf("An error occured while reading the code sent from the server\n");
 		ssl_err = SSL_get_error(cSSL,ssl_err);
 		if(ssl_err == 6){
@@ -231,12 +246,10 @@ int send_reviews(char* ip,char* authenticationKey){
 			close(sock);
 			SSL_CTX_free(sslctx);
 		}
+    persist_queue_reviews(reviews);
 		return 0;
 	}
 
-	printf("\nProduct %s", randomProduct);
-	printf("\nPID %d", id);
-	printf("\nValue %u\n", randomValor);
 
 	ssl_err=SSL_shutdown(cSSL);
 	int count = 1;
@@ -256,6 +269,8 @@ int send_reviews(char* ip,char* authenticationKey){
 
 	if(ssl_err<0){
 		printf("Error in shutdown\n");
+    persist_queue_reviews(reviews);
+    return 0;
 	}else if(ssl_err==1){
 		printf("Client exited gracefully\n");
 	}
@@ -263,6 +278,7 @@ int send_reviews(char* ip,char* authenticationKey){
 	close(sock);
 	SSL_free(cSSL);
 	SSL_CTX_free(sslctx);
+  persist_queue_reviews(reviews);
   return 0;
 }
 
@@ -274,6 +290,18 @@ int main(int argc,char *argv[]){
         printf("You need to pass the Server IP as argument & the client authentication key\n");
         return 0;
     }
-    send_reviews(argv[1],argv[2]);
+    Cominhos c;
+
+  	char temp[256];
+
+  	strncpy(temp,"d6c67b926311591a3faa9bdeaaefad65c6d2e2c011fa00554b038f6c88d5934340307b73e761b68775d4a65ab800c2b0d3b17f48a4d63210c9d406cb01cbb619", sizeof(temp));
+  	strncpy(c.key, temp, sizeof(temp));
+
+    time(&c.timestamp);
+    while(1){
+      Review* reviews_to_persist=get_reviews_to_persist(c);
+			send_reviews("localhost",c,reviews_to_persist);
+			sleep(5);
+		}
     return 0;
 }
