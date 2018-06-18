ac98088d2bd0ebb5aecd9d4cecd2f8b9dcd320ae
João Moreira <1160928@isep.ipp.pt>
2018-06-18 11:59:42 +0100

    Implemented method readCategoryQuestions of class JSONQuestionsReader.

--- core/src/main/java/cdioil/application/utils/JSONQuestionsReader.java
+++ core/src/main/java/cdioil/application/utils/JSONQuestionsReader.java
@@ -1,9 +1,24 @@
 package cdioil.application.utils;
 
 import cdioil.domain.Question;
+import cdioil.files.FileWriter;
+import de.odysseus.staxon.json.JsonXMLConfig;
+import de.odysseus.staxon.json.JsonXMLConfigBuilder;
+import de.odysseus.staxon.json.JsonXMLInputFactory;
+import de.odysseus.staxon.xml.util.PrettyXMLEventWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
 
 /**
  * Class used for importing Questions from a JSON file
@@ -18,6 +33,11 @@ public class JSONQuestionsReader implements QuestionsReader {
     private final File file;
 
     /**
+     * String with the file path of the converted file (from JSON to XML).
+     */
+    private static final String JSON_FILE_PATH = "jsonToXml.xml";
+
+    /**
      * Creates an instance of JSONQuestionsReader, receiving the name of the
      * file to read.
      *
@@ -29,7 +49,42 @@ public class JSONQuestionsReader implements QuestionsReader {
 
     @Override
     public Map<String, List<Question>> readCategoryQuestions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        InputStream input = null;
+        try {
+            input = new FileInputStream(file);
+            StringWriter output = new StringWriter();
+
+            JsonXMLConfig config = new JsonXMLConfigBuilder().multiplePI(false).build();
+
+            //JSON reader
+            XMLEventReader reader = new JsonXMLInputFactory(config).createXMLEventReader(input);
+            //XML writer
+            XMLEventWriter writer = XMLOutputFactory.newInstance().createXMLEventWriter(output);
+
+            //Format output
+            writer = new PrettyXMLEventWriter(writer);
+
+            //Copy reader to writer
+            writer.add(reader);
+
+            reader.close();
+            writer.close();
+            input.close();
+
+            //Write XML content to file
+            FileWriter.writeFile(new File(JSON_FILE_PATH), output.getBuffer().toString());
+        } catch (IOException | XMLStreamException ex) {
+            Logger.getLogger(JSONProductsReader.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(JSONProductsReader.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return new XMLQuestionsReader(file.getName()).readCategoryQuestions();
     }
 
     @Override
