213736e246140eac0a9dae827dd649977a4a3dc8
Joana Pinheiro [1161380] <1161380@isep.ipp.pt>
2018-06-18 10:48:32 +0100

    [see #64] Added the body of the method readIndependentQuestions() from the class JSONQuestionsReader (missing the return)

--- core/src/main/java/cdioil/application/utils/JSONQuestionsReader.java
+++ core/src/main/java/cdioil/application/utils/JSONQuestionsReader.java
@@ -1,21 +1,39 @@
 package cdioil.application.utils;
 
 import cdioil.domain.Question;
-import java.io.File;
+import cdioil.files.FileWriter;
+import de.odysseus.staxon.json.JsonXMLConfig;
+import de.odysseus.staxon.json.JsonXMLConfigBuilder;
+import de.odysseus.staxon.json.JsonXMLInputFactory;
+import de.odysseus.staxon.xml.util.PrettyXMLEventWriter;
+
+import java.io.*;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
 
 /**
  * Class used for importing Questions from a JSON file
  *
  * @author João
+ * @author <a href="1161380@isep.ipp.pt">Joana Pinheiro</a>
  */
 public class JSONQuestionsReader implements QuestionsReader {
 
     /**
-     * File being read.
+     * Path of the file JSON to be imported
      */
     private final File file;
+
+    /**
+     * String with the path of the file converted from JSON to XML
+     */
+    private static final String outputFilePath = "independent_questions_output_file.xml";
 
     /**
      * Creates an instance of JSONQuestionsReader, receiving the name of the
@@ -34,7 +52,41 @@ public class JSONQuestionsReader implements QuestionsReader {
 
     @Override
     public List<Question> readIndependentQuestions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream(file);
+            StringWriter output = new StringWriter();
+
+            JsonXMLConfig jsonXMLConfig = new JsonXMLConfigBuilder().multiplePI(false).build();
+
+            XMLEventReader xmlEventReader = new JsonXMLInputFactory(jsonXMLConfig).createXMLEventReader(input);
+
+            XMLEventWriter xmlEventWriter = XMLOutputFactory.newInstance().createXMLEventWriter(output);
+
+            xmlEventWriter = new PrettyXMLEventWriter(xmlEventWriter);
+
+            xmlEventWriter.add(xmlEventReader);
+
+            xmlEventReader.close();
+            xmlEventWriter.close();
+            input.close();
+
+            FileWriter.writeFile(new File("caminho"), output.getBuffer().toString());
+
+        } catch (XMLStreamException | IOException ex) {
+            Logger.getLogger(JSONProductsReader.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if(input != null) {
+                try {
+                    input.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(JSONProductsReader.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        //return new XMLQuestionReader(...).readIndependentQuestions();
+        return null;
     }
 
 }
