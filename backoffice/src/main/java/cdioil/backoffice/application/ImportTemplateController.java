b7c6a90cd1fa0e9dfed39ccded7acd627d936d3b
António Sousa [1161371] <1161371@isep.ipp.pt>
2018-06-19 14:48:04 +0100

    Add controller implementation for Importing Template from file; Update UI re #51

--- backoffice/src/main/java/cdioil/backoffice/application/ImportTemplateController.java
+++ backoffice/src/main/java/cdioil/backoffice/application/ImportTemplateController.java
@@ -1,173 +1,166 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package cdioil.backoffice.application;
-//
-//import cdioil.application.utils.SurveyTemplate;
-//import cdioil.application.utils.SurveyTemplate.Question;
-//import cdioil.application.utils.SurveyTemplate.BinaryQuestion;
-//import cdioil.application.utils.SurveyTemplate.MultipleChoiceQuestion;
-//import cdioil.application.utils.SurveyTemplate.OnReply;
-//import cdioil.application.utils.SurveyTemplate.OnReplyQuestion;
-//import cdioil.application.utils.SurveyTemplate.Option;
-//import cdioil.application.utils.SurveyTemplate.QuantitiveQuestion;
-//import cdioil.application.utils.SurveyTemplate.SurveyItem;
-//import cdioil.domain.Category;
-//import cdioil.domain.QuantitativeQuestion;
-//import cdioil.domain.QuantitativeQuestionOption;
-//
-//import cdioil.domain.QuestionGroup;
-//import cdioil.domain.QuestionOption;
-//import cdioil.domain.ScriptedTemplate;
-//import cdioil.domain.Template;
-//import cdioil.files.CommonFileExtensions;
-//import java.io.File;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import javax.xml.bind.JAXBContext;
-//import javax.xml.bind.JAXBException;
-//import javax.xml.bind.Unmarshaller;
-//
-///**
-// * Controller for US-236 (import template script from XML file).
-// *
-// * @author António Sousa [1161371]
-// */
+package cdioil.backoffice.application;
+
+import cdioil.application.utils.TemplateReader;
+import cdioil.application.utils.TemplateReaderFactory;
+import cdioil.domain.Category;
+import cdioil.domain.CategoryTemplatesLibrary;
+import cdioil.domain.IndependentTemplatesLibrary;
+import cdioil.domain.Product;
+import cdioil.domain.ProductTemplatesLibrary;
+import cdioil.domain.Question;
+import cdioil.domain.SKU;
+import cdioil.domain.SurveyItem;
+import cdioil.domain.SurveyItemType;
+import cdioil.domain.Template;
+import cdioil.domain.authz.Manager;
+import cdioil.persistence.impl.CategoryTemplatesLibraryRepositoryImpl;
+import cdioil.persistence.impl.IndependentTemplatesLibraryRepositoryImpl;
+import cdioil.persistence.impl.MarketStructureRepositoryImpl;
+import cdioil.persistence.impl.ProductRepositoryImpl;
+import cdioil.persistence.impl.ProductTemplatesLibraryRepositoryImpl;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Controller for US-236 (import template script from XML file).
+ *
+ * @author António Sousa [1161371]
+ */
 public class ImportTemplateController {
-//
-//    /**
-//     * Imports a template script from a XML file.
-//     *
-//     * @param filePath Path of the file to import
-//     * @return true, if the template is successfully imported. Otherwise, returns false.
-//     */
-//    public boolean importTemplate(String filePath) {
-//        if (filePath == null || filePath.isEmpty() || !filePath.endsWith(CommonFileExtensions.XML_EXTENSION)) {
-//            return false;
-//        }
-//
-//        try {
-//            JAXBContext jc = JAXBContext.newInstance(SurveyTemplate.class);
-//
-//            Unmarshaller unmarshaller = jc.createUnmarshaller();
-//            SurveyTemplate surveyTemplate = (SurveyTemplate) unmarshaller.unmarshal(new File(filePath));
-//
-//            String name = surveyTemplate.getName();
-//            List<Question> script = surveyTemplate.getTemplateQuestionScript().getOnReplyQuestions();
-//            List<Question> questions = surveyTemplate.getTemplateQuestions().getQuestions();
-//            List<SurveyItem> surveyItems = surveyTemplate.getTemplateSurveyItems().getSurveyItems();
-//
-//            String path = "";
-//            for (SurveyItem s : surveyItems) {
-//                if (s instanceof cdioil.application.utils.SurveyTemplate.Category) {
-//                    path = ((cdioil.application.utils.SurveyTemplate.Category) s).getPath();
-//                }
+
+    /**
+     * Manager currently logged in.
+     */
+    private final Manager manager;
+
+    /**
+     * Map associating questionID to the respective Question
+     */
+    private final Map<String, Question> questionMap;
+
+    /**
+     * Instantiates the controller.
+     *
+     * @param manager currently logged in manager
+     */
+    public ImportTemplateController(Manager manager) {
+        this.manager = manager;
+        this.questionMap = new LinkedHashMap<>();
+    }
+
+    /**
+     * Creates a template from a given file.
+     *
+     * @param filePath
+     * @return true - if a template was created successfully<p>
+     * false - otherwise
+     * @throws java.io.IOException
+     */
+    public boolean importTemplate(String filePath) throws IOException {
+
+        TemplateReader templateReader = TemplateReaderFactory.create(filePath);
+
+        List<String> questionIDList = templateReader.getDatabaseQuestionIDList();
+
+        Map<SurveyItemType, List<String>> surveyItemIdentifiers = templateReader.getSurveyItemIdentifiers();
+
+        //If no item identifiers are present, use the independent templates library
+        if (surveyItemIdentifiers.isEmpty()) {
+
+            IndependentTemplatesLibraryRepositoryImpl independentTemplatesLibraryRepo = new IndependentTemplatesLibraryRepositoryImpl();
+
+            IndependentTemplatesLibrary independentTemplatesLib = independentTemplatesLibraryRepo.findLibrary();
+
+//            //add fetching by questionID
+//            for (String questionID : questionIDList) {
+//                //templateReader.addDatabaseQuestion(q);
 //            }
-//
-//            if (path.isEmpty()) {
-//                return false;
+            Template template = templateReader.createTemplate();
+
+            if (!independentTemplatesLib.addTemplate(template)) {
+                return false;
+            }
+
+            return independentTemplatesLibraryRepo.merge(independentTemplatesLib) != null;
+
+        } else {
+
+            List<SurveyItem> associatedItems = new LinkedList<>();
+
+            MarketStructureRepositoryImpl marketStructureRepo = new MarketStructureRepositoryImpl();
+            ProductRepositoryImpl productRepo = new ProductRepositoryImpl();
+
+            for (Map.Entry<SurveyItemType, List<String>> entry : surveyItemIdentifiers.entrySet()) {
+
+                SurveyItemType itemType = entry.getKey();
+
+                List<String> itemIdentifiers = entry.getValue();
+
+                if (itemType == SurveyItemType.CATEGORY) {
+
+                    for (String path : itemIdentifiers) {
+                        Category c = marketStructureRepo.findCategoryByPath(path);
+
+                        if (c == null) {
+                            return false;
+                        }
+
+                        if (!manager.isAssociatedWithCategory(c)) {
+                            return false;
+                        }
+                        associatedItems.add(c);
+                    }
+                } else if (itemType == SurveyItemType.PRODUCT) {
+
+                    for (String sku : itemIdentifiers) {
+
+                        Product p = productRepo.getProductBySKU(new SKU(sku));
+
+                        if (p == null) {
+                            return false;
+                        }
+
+                        associatedItems.add(p);
+                    }
+                }
+            }
+
+//            //add fetching by questionID
+//            for (String questionID : questionIDList) {
+//                //templateReader.addDatabaseQuestion(q);
 //            }
-//
-//            String questionGroupName = path + " Group";
-//            QuestionGroup questionGroup = new QuestionGroup(questionGroupName);
-//
-//            for (Question q : questions) {
-//                String questionText = q.getText();
-//                String questionID = q.getID();
-//                if (q instanceof cdioil.application.utils.SurveyTemplate.QuantitiveQuestion) {
-//                    cdioil.application.utils.SurveyTemplate.QuantitiveQuestion quantitaiveQuestion = (cdioil.application.utils.SurveyTemplate.QuantitiveQuestion) q;
-//
-//                    List<QuestionOption> optionList = new ArrayList<>();
-//                    for (double scaleValue = quantitaiveQuestion.getMaximumValueScale(); scaleValue <= quantitaiveQuestion.getMaximumValueScale(); scaleValue++) {
-//                        cdioil.domain.QuestionOption option = new cdioil.domain.QuantitativeQuestionOption(scaleValue);
-//                        optionList.add(option);
-//                    }
-//                    cdioil.domain.QuantitativeQuestion question = new cdioil.domain.QuantitativeQuestion(questionText, questionID, optionList);
-//                    questionGroup.addQuestion(question);
-//
-//                } else if (q instanceof cdioil.application.utils.SurveyTemplate.BinaryQuestion) {
-//                    cdioil.domain.BinaryQuestion question = new cdioil.domain.BinaryQuestion(questionText, questionID);
-//                    questionGroup.addQuestion(question);
-//
-//                } else if (q instanceof cdioil.application.utils.SurveyTemplate.MultipleChoiceQuestion) {
-//                    cdioil.application.utils.SurveyTemplate.MultipleChoiceQuestion multipleChoiceQuestion = (cdioil.application.utils.SurveyTemplate.MultipleChoiceQuestion) q;
-//                    List<QuestionOption> optionList = new ArrayList<>();
-//                    for (Option op : multipleChoiceQuestion.getOptionsList()) {
-//                        QuestionOption option = new cdioil.domain.MultipleChoiceQuestionOption(op.getOptionText());
-//                    }
-//                    cdioil.domain.MultipleChoiceQuestion question = new cdioil.domain.MultipleChoiceQuestion(questionText, questionID, optionList);
-//                    questionGroup.addQuestion(question);
-//                }
-//            }
-//
-//            ScriptedTemplate template = new ScriptedTemplate(name, questionGroup);
-//
-//            for (Question q : script) {
-//                if (q instanceof cdioil.application.utils.SurveyTemplate.OnReplyQuestion) {
-//
-//                    OnReplyQuestion onReplyQuestion = (OnReplyQuestion) q;
-//
-//                    OnReply reply = onReplyQuestion.getReplyData();
-//                    OnReplyQuestion nextQuestion = reply.getNextQuestion();
-//                    String options = reply.getOnReplyOptions();
-//
-//                    for (cdioil.domain.Question question : questionGroup.getQuestions()) {
-//                        cdioil.domain.Question origin, next;
-//                        if (q.getID().equals(question.getQuestionID())) {
-//                            origin = question;
-//                        }
-//
-//                        if (nextQuestion.getID().equals(q.getID())) {
-//                            next = question;
-//                        }
-//                        
-//                        
-//                        
-//                    }
-//
-//                    cdioil.application.utils.SurveyTemplate.QuantitiveQuestion quantitaiveQuestion = (cdioil.application.utils.SurveyTemplate.QuantitiveQuestion) q;
-//
-//                    List<QuestionOption> optionList = new ArrayList<>();
-//                    for (double scaleValue = quantitaiveQuestion.getMaximumValueScale(); scaleValue <= quantitaiveQuestion.getMaximumValueScale(); scaleValue++) {
-//                        cdioil.domain.QuestionOption option = new cdioil.domain.QuantitativeQuestionOption(scaleValue);
-//                        optionList.add(option);
-//                    }
-//                    cdioil.domain.QuantitativeQuestion question = new cdioil.domain.QuantitativeQuestion(questionText, questionID, optionList);
-//
-//                    if (q instanceof cdioil.application.utils.SurveyTemplate.QuantitiveQuestion) {
-//                        cdioil.application.utils.SurveyTemplate.QuantitiveQuestion quantitaiveQuestion2 = (cdioil.application.utils.SurveyTemplate.QuantitiveQuestion) q;
-//
-//                        List<QuestionOption> optionList2 = new ArrayList<>();
-//                        for (double scaleValue = quantitaiveQuestion2.getMaximumValueScale(); scaleValue <= quantitaiveQuestion2.getMaximumValueScale(); scaleValue++) {
-//                            cdioil.domain.QuestionOption option = new cdioil.domain.QuantitativeQuestionOption(scaleValue);
-//                            optionList2.add(option);
-//                        }
-//                        cdioil.domain.QuantitativeQuestion question23232 = new cdioil.domain.QuantitativeQuestion(questionText, questionID, optionList2);
-//                        questionGroup.addQuestion(question23232);
-//
-//                    } else if (q instanceof cdioil.application.utils.SurveyTemplate.BinaryQuestion) {
-//                        cdioil.domain.BinaryQuestion heiufudsfniofio = new cdioil.domain.BinaryQuestion(questionText, questionID);
-//                        questionGroup.addQuestion(heiufudsfniofio);
-//
-//                    } else if (q instanceof cdioil.application.utils.SurveyTemplate.MultipleChoiceQuestion) {
-//                        cdioil.application.utils.SurveyTemplate.MultipleChoiceQuestion multipleChoiceQuestion = (cdioil.application.utils.SurveyTemplate.MultipleChoiceQuestion) q;
-//                        List<QuestionOption> sdfkjdnfjdsfd = new ArrayList<>();
-//                        for (Option op : multipleChoiceQuestion.getOptionsList()) {
-//                            QuestionOption option = new cdioil.domain.MultipleChoiceQuestionOption(op.getOptionText());
-//                        }
-//                        cdioil.domain.MultipleChoiceQuestion bfudsfbkdsfhkdsf = new cdioil.domain.MultipleChoiceQuestion(questionText, questionID, sdfkjdnfjdsfd);
-//                        questionGroup.addQuestion(bfudsfbkdsfhkdsf);
-//                    }
-//                }
-//            }
-//
-//        } catch (JAXBException ex) {
-//            Logger.getLogger(ImportTemplateController.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return false;
-//    }
+            Template template = templateReader.createTemplate();
+
+            //Note: This solution is not ideal since the libraries need to be completely loaded
+            CategoryTemplatesLibraryRepositoryImpl catTempLibRepo = new CategoryTemplatesLibraryRepositoryImpl();
+            CategoryTemplatesLibrary catTempLib = catTempLibRepo.findTemplatesForCategory();
+
+            ProductTemplatesLibraryRepositoryImpl prodTempLibRepo = new ProductTemplatesLibraryRepositoryImpl();
+            ProductTemplatesLibrary prodTempLib = prodTempLibRepo.findLibrary();
+
+            for (SurveyItem surveyItem : associatedItems) {
+
+                boolean added = false;
+
+                if (surveyItem instanceof Category) {
+
+                    added = catTempLib.addTemplate((Category) surveyItem, template);
+
+                } else if (surveyItem instanceof Product) {
+
+                    added = prodTempLib.addTemplate((Product) surveyItem, template);
+                }
+
+                if (!added) {
+                    return false;
+                }
+            }
+
+            return prodTempLibRepo.merge(prodTempLib) != null && catTempLibRepo.merge(catTempLib) != null;
+        }
+    }
+
 }
