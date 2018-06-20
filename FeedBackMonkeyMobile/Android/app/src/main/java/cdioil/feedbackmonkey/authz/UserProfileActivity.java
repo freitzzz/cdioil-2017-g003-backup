ec4821635ffcabc336e7bfe4eeb925a7c8a10b6b
Gil Durão <1160936@isep.ipp.pt>
2018-06-20 02:45:31 +0100

    UserProfileActivity now fetches the list of surveys that a user has fully answered and starts ListSurveyActivity with the referred list see #58

--- FeedBackMonkeyMobile/Android/app/src/main/java/cdioil/feedbackmonkey/authz/UserProfileActivity.java
+++ FeedBackMonkeyMobile/Android/app/src/main/java/cdioil/feedbackmonkey/authz/UserProfileActivity.java
@@ -28,8 +28,15 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import cdioil.feedbackmonkey.R;
+import cdioil.feedbackmonkey.application.ListSurveyActivity;
+import cdioil.feedbackmonkey.application.services.SurveyService;
+import cdioil.feedbackmonkey.application.services.SurveyServiceController;
+import cdioil.feedbackmonkey.restful.exceptions.RESTfulException;
 import cdioil.feedbackmonkey.utils.GenericFileProvider;
 import cdioil.feedbackmonkey.utils.ToastNotification;
 
@@ -83,6 +90,10 @@ public class UserProfileActivity extends AppCompatActivity {
      * String that holds the profile picture path.
      */
     private String pictureImagePath;
+    /**
+     * String that holds the users authenticationToken.
+     */
+    private String authenticationToken;
 
     /**
      * Activity will execute this code when its created
@@ -108,6 +119,7 @@ public class UserProfileActivity extends AppCompatActivity {
         reviewedSurveysButton = findViewById(R.id.reviewListButton);
         contestBadgeListButton = findViewById(R.id.contestBadgeListButton);
         suggestionListButton = findViewById(R.id.suggestionListButton);
+        authenticationToken = getIntent().getExtras().getString("authenticationToken");
         configureImageAndTextViews();
         configureButtons();
     }
@@ -131,15 +143,66 @@ public class UserProfileActivity extends AppCompatActivity {
      * Configures the buttons of this activity.
      */
     private void configureButtons() {
-        reviewedSurveysButton.setOnClickListener(view ->{
-            //TODO Create Intent to activity that will show the users reviews.
+        reviewedSurveysButton.setOnClickListener(view -> {
+            prepareListSurveyActivityStart();
         });
-        contestBadgeListButton.setOnClickListener(view ->{
-            ToastNotification.show(this,getString(R.string.functionality_not_implemented));
+        contestBadgeListButton.setOnClickListener(view -> {
+            ToastNotification.show(this, getString(R.string.functionality_not_implemented));
         });
-        suggestionListButton.setOnClickListener(view ->{
-            //TODO Create Intent to activity that will show the users suggestion list.
+        suggestionListButton.setOnClickListener(view -> {
+            ToastNotification.show(this, getString(R.string.functionality_not_implemented));
         });
+    }
+
+    /**
+     * Runs a new Thread that prepares the list survey activity start.
+     */
+    private void prepareListSurveyActivityStart() {
+        new Thread(fetchUserAnsweredSurveys()).start();
+    }
+
+    /**
+     * Fetches the user answered survey list.
+     *
+     * @return Runnable that executes a REST Request to get all the surveys that have been fully answered by a user
+     */
+    private Runnable fetchUserAnsweredSurveys() {
+        return () -> {
+            try {
+                List<SurveyService> surveyServiceList = new SurveyServiceController(authenticationToken).getUserAnsweredSurveys((short) 0);
+                startListSurveyActivity(surveyServiceList);
+            } catch (RESTfulException restfulException) {
+                switch (restfulException.getCode()) {
+                    case HttpsURLConnection.HTTP_BAD_REQUEST:
+                        ToastNotification.show(UserProfileActivity.this, "Ainda não realizou nenhuma avaliação.");
+                        break;
+                    case HttpsURLConnection.HTTP_UNAUTHORIZED:
+                        //TODO: Authentication Token is not valid anymore, Go back to LoginActivity
+                        break;
+                    default:
+                        ToastNotification.show(UserProfileActivity.this, "Erro de conexão ao servidor, tente mais tarde");
+                }
+            } catch (IOException e) {
+                ToastNotification.show(UserProfileActivity.this, getString(R.string.no_internet_connection));
+            }
+        };
+    }
+
+    /**
+     * Creates an intent to start ListSurveyActivity
+     *
+     * @param surveyServiceList list of the surveys a user has fully answered that are to be listed in ListSurveyActivity
+     */
+    private void startListSurveyActivity(List<SurveyService> surveyServiceList) {
+        Intent listSurveyActivityIntent = new Intent(UserProfileActivity.this, ListSurveyActivity.class);
+        Bundle bundle = new Bundle(surveyServiceList.size() + 1);
+        bundle.putString("authenticationToken", authenticationToken);
+        bundle.putString("sentFromProfileActivity", this.getClass().getSimpleName());
+        for (int i = 0; i < surveyServiceList.size(); i++) {
+            bundle.putSerializable("" + i, surveyServiceList.get(i));
+        }
+        listSurveyActivityIntent.putExtras(bundle);
+        startActivity(listSurveyActivityIntent);
     }
 
     /**
@@ -156,11 +219,11 @@ public class UserProfileActivity extends AppCompatActivity {
                     String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
                     String imageFileName = timeStamp + ".jpg";
                     File storageDir = getFilesDir();
-                    File imageDir = new File(storageDir,"images");
-                    if(!imageDir.exists()){
+                    File imageDir = new File(storageDir, "images");
+                    if (!imageDir.exists()) {
                         imageDir.mkdir();
                     }
-                    File imageFile = new File(imageDir,imageFileName);
+                    File imageFile = new File(imageDir, imageFileName);
                     try {
                         imageFile.createNewFile();
                     } catch (IOException e) {
@@ -168,7 +231,7 @@ public class UserProfileActivity extends AppCompatActivity {
                     }
                     pictureImagePath = imageFile.getAbsolutePath();
                     Uri outputFileUri = GenericFileProvider.getUriForFile(getApplicationContext(),
-                                    "cdioil.feedbackmonkey.utils.GenericFileProvider",imageFile);
+                            "cdioil.feedbackmonkey.utils.GenericFileProvider", imageFile);
                     Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     cameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
@@ -206,20 +269,20 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             File imageFile = new File(pictureImagePath);
-            if(imageFile.exists()){
+            if (imageFile.exists()) {
                 Bitmap imageBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
                 try {
                     ExifInterface exifInterface = new ExifInterface(imageFile.getAbsolutePath());
-                    int orientation =  exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,
                             1);
                     Matrix matrix = new Matrix();
-                    if(orientation == 8){
+                    if (orientation == 8) {
                         matrix.postRotate(270);
-                    }else if(orientation == 6){
+                    } else if (orientation == 6) {
                         matrix.postRotate(90);
                     }
-                    Bitmap rotatedImageBitmap = Bitmap.createBitmap(imageBitmap,0,0,
-                            imageBitmap.getWidth(),imageBitmap.getHeight(),matrix,true);
+                    Bitmap rotatedImageBitmap = Bitmap.createBitmap(imageBitmap, 0, 0,
+                            imageBitmap.getWidth(), imageBitmap.getHeight(), matrix, true);
                     profilePhotoImageView.setImageBitmap(rotatedImageBitmap);
                 } catch (IOException e) {
                     e.printStackTrace();
