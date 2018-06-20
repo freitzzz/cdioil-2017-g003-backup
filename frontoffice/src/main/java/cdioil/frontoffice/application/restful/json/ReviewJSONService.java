5c21c88444f2e4fcc1cac2895e7e5f7ab7389be0
Gil Durão <1160936@isep.ipp.pt>
2018-06-20 11:54:19 +0100

    Refactored ReviewJSONService class, added AnswerJSONService and QuestionJSONService see #59

--- frontoffice/src/main/java/cdioil/frontoffice/application/restful/json/ReviewJSONService.java
+++ frontoffice/src/main/java/cdioil/frontoffice/application/restful/json/ReviewJSONService.java
@@ -1,53 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cdioil.frontoffice.application.restful.json;
 
+import cdioil.application.utils.services.json.AnswerJSONService;
+import cdioil.application.utils.services.json.QuestionJSONService;
 import cdioil.domain.Answer;
 import cdioil.domain.Question;
 import com.google.gson.annotations.SerializedName;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 /**
- * JSON Service that represents a user that is reviewing a product (answering a survey).
- *
- * @author <a href="1160912@isep.ipp.pt">Rita Gonçalves</a>
- * @author <a href="1161380@isep.ipp.pt">Joana Pinheiro</a>
- *
- * @since Version 5.0 of FeedbackMonkey
+ * ReviewJSONService class that represents the serialization/deserialization of a Review 
+ * in JSON object format
+ * @author <a href="1160907@isep.ipp.pt">João Freitas</a>
+ * @since Version 6.0 of FeedbackMonkey
  */
-public class ReviewJSONService {
+public final class ReviewJSONService {
     /**
-     * Question Answer map of a review.
+     * Map with all the review answers in Question-Answers pair value format
      */
-    @SerializedName(value = "questionAsnwerMap", alternate = {"QUESTIONANSWERMAP", "questionanswermap"})
-    private Map<String,String> questionAnswerMap;
-    
+    @SerializedName(value="reviewAnswers",alternate = {"ReviewAnswers","REVIEWANSWERS"})
+    private Map<QuestionJSONService,AnswerJSONService> reviewAnswers;
     /**
-     * Builds a new ReviewJSONService with the question answer map of the review
-     * being serialized
-     * 
-     * @param questionAnswerMap  question answer map of the review being serialized
+     * String with the Review suggestion
      */
-    public ReviewJSONService(Map<String,String> questionAnswerMap){
-        this.questionAnswerMap = new HashMap<>();
-        questionAnswerMap.entrySet().forEach((entry) -> {
-            this.questionAnswerMap.put(entry.getKey(),entry.getValue());
-        });
+    @SerializedName(value="suggestion",alternate = {"Suggestion","SUGGESTION"})
+    private String suggestion;
+    /**
+     * Builds a new ReviewJSONService with the review answers in Question-Answer 
+     * pair format being serialiazed into a JSON object
+     * @param reviewAnswers Map with the reviews answers in Question-Answers pair 
+     * format being serialized into a JSON object
+     * @param suggestion String with the review suggestion
+     */
+    public ReviewJSONService(Map<Question,Answer> reviewAnswers,String suggestion){
+        this.reviewAnswers=toJSONServiceMap(reviewAnswers);
+        this.suggestion=suggestion;
     }
-
     /**
-     * Access method to the question answer map.
+     * Method that converts a Map of Question-Answer pair values into a Map of QuestionJSONService-AnswerJSONService
+     * @param reviewAnswers Map with the review answers in Question-Answer pair value format being converted 
+     * to QuestionJSONService-AnswerJSONService pair value format
+     * @return Map with the review answers converted into a QuestionJSONService-AnswerJSONService pair value 
+     * format
+     */
+    private Map<QuestionJSONService,AnswerJSONService> toJSONServiceMap(Map<Question,Answer> reviewAnswers){
+        Map<QuestionJSONService,AnswerJSONService> reviewAnswersJSONService=new HashMap<>();
+        reviewAnswers.forEach((question,answer)->{
+            reviewAnswersJSONService.put(new QuestionJSONService(question),new AnswerJSONService(answer));
+        });
+        return reviewAnswersJSONService;
+    }
+    /**
+     * Builds a Map<String,String> from a Map<QuestionJSONService,AnswerJSONService>
      * 
-     * @return map with the questions and answers of a review
+     * @return question answer map of a user's review
      */
     public Map<String,String> getQuestionAnswerMap(){
-        return questionAnswerMap;
+        Map<String,String> questionAnswerStringMap = new HashMap<>();
+        reviewAnswers.entrySet().forEach((entry) -> {
+            questionAnswerStringMap.put(entry.getKey().getQuestionText(),
+                    entry.getValue().getAnswer());
+        });
+        return questionAnswerStringMap;
     }
 }
