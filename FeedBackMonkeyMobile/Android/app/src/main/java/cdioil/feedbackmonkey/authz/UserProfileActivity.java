635ef1459e2722eb44e4b5c268fb7ce5bac2fa9d
João Pedro Magalhães Freitas <1160907@isep.ipp.pt>
2018-06-22 18:21:50 +0100

    Merge branch

--- FeedBackMonkeyMobile/Android/app/src/main/java/cdioil/feedbackmonkey/authz/UserProfileActivity.java
+++ FeedBackMonkeyMobile/Android/app/src/main/java/cdioil/feedbackmonkey/authz/UserProfileActivity.java
@@ -216,14 +216,13 @@ public class UserProfileActivity extends AppCompatActivity {
                 if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     requestPermissions(new String[]{Manifest.permission.CAMERA},
                             MY_CAMERA_PERMISSION_CODE);
-                } else {
-                    Uri outputFileUri = getOutputFileUri();
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    cameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
                 }
             }
+            Uri outputFileUri = getOutputFileUri();
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
         });
     }
 
